4def0a540063456e9ce31b196491291712a61387
Пользователь <mdylll@mail.ru>
2019-03-16 13:05:49 +0300

    from Martynov

--- basics/core.js
+++ basics/core.js
@@ -1,67 +1,72 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
-    return (n^0)===n;
+    return (n ^ 0) === n;
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
-    let arr=[];
-    for (let i=0;i<10;i++){
-        arr[i]=2*(i+1);
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+        arr[i] = 2 * (i + 1);
     }
     return arr;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
-    let sum=0;
-    for (let i=1;i<=n;i++){
-        sum+=i;
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
     }
     return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
-    if (n==1){
-        return 1;
-    }
-    else return n+recSumTo(n-1);
+    if (n < 0 || (n ^ 0) !== n)
+        return null;
+    if (n == 0) {
+        return 0;
+    } else return n + recSumTo(n - 1);
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
-    if (n==1){
+    if (n == 1 || n == 0) {
         return 1;
     }
-    else
-        return n*factorial(n-1);
+    return n * factorial(n - 1);
 }
 
 //Напишите функцию, которая определяет, является ли число бинарным
 function isBinary(n) {
-    if (n==0){
+    if (n == 0) {
         return false;
     }
-    while (n!=1){
-        if (n%2==1)
+    while (n != 1) {
+        if (n % 2 == 1) {
             return false;
-        else
-            n=n/2;
+        }
+        n = n / 2;
     }
     return true;
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
-    if (n==1||n==2) {
+    if (n==1){
         return 1;
     }
-    else {
-        return fibonacci(n-1)+fibonacci(n-2);
+    let fib2=(a,b,num)=>{
+        if (num==n){
+            return b;
+        }
+        return fib2(b,a+b,num+1);
     }
+    return fib2(1,1,2)
 }
 
 module.exports = {
     isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
 }
+
