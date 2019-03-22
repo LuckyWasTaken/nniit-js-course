eb9929ab0522a05f5a3e761480b8b5d323ab71bb
MDYLL <mdylll@mail.ru>
2019-03-22 23:34:07 +0300

    structures from Martynov

--- closures/core.js
+++ closures/core.js
@@ -1,12 +1,26 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
 function sum(a) {
+  let currentSum=a;
+  return function (b) {
+    currentSum+=b;
+    return currentSum;
+    }
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+  let currentCount=0;
+  let currentSum=0;
+  return function(a){
+    currentSum+=a;
+    currentCount++;
+    return currentSum/currentCount;
+  }
 }
 
 module.exports = {
   sum,
   average
 };
+
+console.log(sum(4)(5));
