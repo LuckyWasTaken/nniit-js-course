496f97e1725cd8ce6aab9177d2cabc1c2f310ef6
AlexandrMonakhov <zabuldigaa@gmail.com>
2019-03-26 00:50:58 +0300

    Second homework v.1

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,27 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
-function sum(a) {
+function sum(a) { 
+  let currSum = a;
+
+  function f(b) {
+    currSum += b;
+    return f;
+  }
+
+  f.toString = function() {
+    return currentSum;
+  };
+
+  return f;
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+  let sum = 0;
+  let count = 0;
+  return function(num) {
+    sum += num;
+    return sum/++count;
+  };
 }
 
 module.exports = {
