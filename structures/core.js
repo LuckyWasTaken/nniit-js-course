c7b642eaa58347eb4fae99fbc853df56dccabf3c
Denis Gandzha <gandja.denis@yandex.ru>
2019-03-26 23:19:04 +0300

    "Changed structures"

--- structures/core.js
+++ structures/core.js
@@ -4,12 +4,10 @@ function capitalize(str) {
  if(!str || typeof str !== "string"){
     return "";
  }
- else {
-    let arrayOfString = [];
-       arrayOfString = str.split(' ');
+    let arrayOfString = arrayOfString = str.split(' ');
     for(let i = 0; i < arrayOfString.length; i++){
        arrayOfString[i] = arrayOfString[i].charAt(0).toUpperCase() + arrayOfString[i].slice(1);
-    }
+
     return arrayOfString;
  }
 
@@ -17,43 +15,42 @@ function capitalize(str) {
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
 function truncate(str, n) {
+    if(!str || typeof str !== "string" || !n || typeof n !== "number"){
+        return null;
+    }
     if(str.length < n){
         return str;
     }
-    else if(!str || typeof str !== "string" || !n || typeof n !== "number"){
-        return "Incorrect input";
-    }
-    else {
-        return str = str.substring(0,n) + "...";
-    }
+
+    return str = str.substring(0,n) + "...";
+
 }
 
 //Определите, пуст ли объект
 function isEmpty(obj){
   if(!obj) {
-      return "Incorrect input"
+      throw new Error();
   }
-  else{
-      for( let key in obj){
-          return false;
-      }
-      return true;
+
+  for( let key in obj){
+      return false;
   }
+  return true;
+
 
 }
 
 //Напишите функцию, умножающую численные свойства на 2
 function multiply(obj) {
     if(!obj) {
-        return "Incorrect input"
+        return null;
     }
-    else {
-        for(let key in obj){
-            if(!isNaN(parseFloat(obj[key])) && isFinite(obj[key])){
-                obj[key] *= 2;
+    for(let key in obj){
+        if(!isNaN(parseFloat(obj[key]))){
+            obj[key] *= 2;
             }
         }
-    }
+
     return obj;
 
 }
@@ -69,14 +66,14 @@ function sumArr(arr) {
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
 function isPali(str) {
-    if(!str || typeof str !== "string"){
-        return "Incorrect input";
+    if(typeof str !== "string"){
+        return false;
     }
     let strReverse = str.split("").reverse().join("");
     return str == strReverse;
 
 }
-console.log(sumArr([1,2,3]));
+
 module.exports = {
   capitalize,
   truncate,
