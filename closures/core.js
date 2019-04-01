afd173f692944d03c8cb9479a7242aca6c86acdc
ipetrovanton <ipetrovanton@gmail.com>
2019-04-01 09:53:00 +0300

    Petrov. Lesson-2

--- closures/core.js
+++ closures/core.js
@@ -1,12 +1,34 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
-function sum(a) {
+function sum(a) { // работает с любым количеством чисел
+	let value = a;
+	function subSum(b) {
+		return sum(a + b);
+	}
+	subSum.toString = subSum.valueOf = function() {
+		return value;
+	};
+	return subSum;
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+	let result = 0;
+	let counter = 0;
+	return function(number) {
+		result += number;
+		counter++;
+		return result / counter;
+	};
 }
+const avg = average();
+
 
 module.exports = {
-  sum,
-  average
+	sum,
+	average,
 };
+
+
+
+
+
--- structures/core.js
+++ structures/core.js
@@ -1,26 +1,63 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+    if (!str) return str;
+    let newStr = str.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
+    return newStr;
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+	if(n > str.length)
+	return str;
+	let newStr = str.substr(0, n) + "...";
+	return newStr;	
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+	for(let key in obj){
+		return false;
+	}
+	return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+	for(let key in obj){
+		if(isNumber(obj[key])){
+			obj[key] *= 2;
+		}
+	}
+	return obj;
+}
+function isNumber (n){
+	return !isNaN(parseFloat(n) && isFinite(n));
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+	return arr.reduce(function(sum, current) {
+		return sum + current;
+	}, 0);
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+	for (let i = 0; i < str.length / 2; i++) {
+		if (str[i] != str[str.length - 1 - i]) {
+			return false;
+		}
+	}
+	return true;
+}
 
 module.exports = {
-  capitalize,
-  truncate,
-  isEmpty,
-  multiply,
-  sumArr,
-  isPali
+	capitalize,
+	truncate,
+	isEmpty,
+	multiply,
+	sumArr,
+	isPali,
 };
+
+
