b732b95a5feefea39e026fd28c29a8b38d810992
lerkas <44975578+lerkas@users.noreply.github.com>
2019-04-04 14:07:01 +0300

    Update core.js

--- basics/core.js
+++ basics/core.js
@@ -1,61 +1,29 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
-function isInteger(n){
-	return (~~n) === n;
+function isInteger(n) {
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
-function even(){
-	let first, i = 0;
-	let buf = [];
-	for(first = 2; first <= 20; first++){
-		if(first % 2 === 0){
-			buf[i] = first;
-			i++;
-		}
-	}
-	return buf;
+function even() {
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
-function sumTo(n){
-	let sum = 0;
-	for(let i = 0; i <= n; i++){
-		sum += i;
-	}
-	return sum;
+function sumTo(n) {
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
-function recSumTo(n){
-	if(n <= 0)
-		return null;
-	return n + recSumTo(n - 1);
+function recSumTo(n) {
 }
 
 //Напишите функцию, считающую факториал заданного числа
-function factorial(n){
-	if(n === 0)
-		return 1;
-	return n * factorial(n - 1);
+function factorial(n) {
 }
 
-//Напишите функцию, которая определяет, является ли число бинарным
-function isBinary(n){
-	if((n != 0) && ((n & (n - 1)) === 0))
-		return true;
-	else
-		return false;
+//Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
+function isBinary(n) {
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
-function fibonacci(n){
-	let a = 1, b = 1, c;
-	for(let i = 3; i <= n; i++){
-		c = a + b;
-		a = b;
-		b = c;
-	}
-	return b;
+function fibonacci(n) {
 }
 
 module.exports = {
