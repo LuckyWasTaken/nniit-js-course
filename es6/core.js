a00275ebb0ae8d93e142bae595d51cccb3b03c72
MDYLL <mdylll@mail.ru>
2019-04-08 18:12:28 +0300

    es6

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,101 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(fio) {
+    let [...data] = fio.split(' ');
+    return `${data[1]} ${data[0]}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arr) {
+    let ans = new Set();
+    arr.forEach(item => {
+        ans.add(item);
+    });
+    return [...ans];
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arr) {
+    let minSalary = arr.reduce((min, current) => {
+        return (current < min ? current : min);
+    }, arr[0]);
+    let maxSalary = arr.reduce((max, current) => {
+        return (current > max ? current : max);
+    }, arr[0]);
+
+    return maxSalary / minSalary || false;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(arr) {
+    ans = [];
+    arr.forEach((item, i) => {
+        ans[i] = item % 15 == 0 ? "FooBar" : item % 3 == 0 ? "Foo" : item % 5 == 0 ? "Bar" : "";
+    });
+    return ans;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor(){
+        this._map=new Map();
+    }
+
+    setValue(a, b) {
+        this._map.set(a,b);
+    }
+
+    getValue(a) {
+        return this._map.get(a);
+    }
+
+    change(a, b) {
+        if (this._map.get(a)) {
+            this.setValue(a, b);
+            return true;
+        }
+        return false;
+    }
+
+    del(a) {
+        return this._map.delete(a);
+    }
+
+}
 
 module.exports = {
     fioToName,
     filterUnique,
     Dictionary,
-    calculateSalaryDifference
+    calculateSalaryDifference,
+    fooBar
 };
+
+d=new Dictionary();
+d.setValue("JS",true);
+console.log(d.getValue("JS"));
+console.log(d.change("JAVA", false));
+console.log(d.change("JS", "very good"));
+d.setValue("Python",3.5);
+console.log(d.getValue("Python"));
+d.del("Python");
+console.log(d.getValue("Python"));
+
+
+
+
+
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -36,11 +36,44 @@ describe('es6', () => {
         });
     });
 
+    describe('#fooBar', () => {
+        it('переводит верно', () => {
+            assert.deepEqual(core.fooBar([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]), ['', '', 'Foo', '', 'Bar',
+                'Foo', '', '', 'Foo', 'Bar', '', 'Foo', '', '', 'FooBar']);
+        });
+        it('на пустой массив возвращается пустой массив', () => {
+            assert.deepEqual(core.fooBar([]), []);
+        });
+    });
+
     describe('#Dictionary', () => {
         it('экземпляр класса создается', () => {
             const dic = new core.Dictionary();
 
             assert.equal(!!dic, true);
         });
+        it('слово добавляется и находится', () => {
+            let d = new core.Dictionary();
+            d.setValue("JS", true);
+
+            assert.equal(d.getValue("JS"), true);
+        })
+
+        it('меняется значение слова', () => {
+            let d = new core.Dictionary();
+            d.setValue("JS", true);
+            d.change("JS", "very good");
+
+            assert.equal(d.getValue("JS"), "very good");
+        })
+
+        it('слово удаляется, если есть', () => {
+            let d = new core.Dictionary();
+            d.setValue("JS", true);
+            assert.equal(d.del("JS"), true);
+            assert.equal(d.del("JS+"), false);
+            d.del("JS");
+            assert.equal(d.getValue("JS"), undefined);
+        })
     });
 });
