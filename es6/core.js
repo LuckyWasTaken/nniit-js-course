d9a907738cbb9095ee0130556e4a4e920a5fe312
alekseypivovar <you@example.com>
2019-04-09 21:36:43 +0300

    Pivovar es6

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,145 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(string) {
+  if (!string || typeof string !== "string") {
+    console.log("Incorrect name!");
+    return undefined;
+  }
+  let [lastName, firstName, fathersName, ...rest] = string.split(" ");
+  return `${firstName} ${lastName}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(valueArray) {
+  if (valueArray.length === 0 || valueArray.isArray == false) {
+    console.log("Incorrect value array!");
+    return [];
+    // Пустой массив таким образом нормально возвращать?
+  }
+  let set = new Set();
+  valueArray.forEach(function(item, i, valueArray) {
+    set.add(item);
+  });
+  return Array.from(set);
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(salaryArray) {
+  if (salaryArray.length === 0 || salaryArray.isArray == false) {
+    console.log("Incorrect salary array!");
+    return false;
+  }
+
+  let minSalary = salaryArray.reduce(function(previousValue, currentValue) {
+    return previousValue < currentValue ? previousValue : currentValue;
+  });
+
+  let maxSalary = salaryArray.reduce(function(previousValue, currentValue) {
+    return previousValue > currentValue ? previousValue : currentValue;
+  });
+
+  return maxSalary / minSalary;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+  if (n <= 0 || isNaN(n) === true) {
+    return [];
+  }
+  var arr = [];
+
+  for (let i = 1; i < n; i++) {
+    if (i % 15 === 0) {
+      arr.push("FooBar");
+    } else if (i % 5 === 0) {
+      arr.push("Bar");
+    } else if (i % 3 === 0) {
+      arr.push("Foo");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+  constructor() {
+   this._map = new Map();
+    
+  }
+  //Добавить слово и его толкование в словарь:
+  addToDictionary(word, meaning) {
+    if (this._checkOnString(word)===false || this._checkOnString(meaning)===false) {
+      console.log("Error! Incorrect arguments for addToDictionary");
+      return false;
+    }
+    this._map.set(word,meaning);
+  }
+
+//Изменить толкование слова:
+  changeMeaning(word, meaning){
+   if (this._map.has(word)===false){
+      console.log("Error! Could not find entered word in dictionary (for set meaning)");
+      return false;
+    }
+   if (this._checkOnString(meaning)===false){
+    console.log("Error! Incorrect meaning entered");
+    return false;
+   }
+   this._map.set(word,meaning);
+  }
+
+  //Проверка является ли слово непустой строкой:
+  _checkOnString(checkingWord){
+    if (
+      !checkingWord ||
+      typeof checkingWord !== "string"
+       ) {
+      return false;
+    }
+    return true;
+  }
+
+  //Удалить слово из словаря:
+  deleteWord(word){
+    if (this._map.has(word)===false){
+      console.log("Error! Could not find entered word in dictionary (to delete)");
+      return false;
+    }
+    this._map.delete(word);
+  }
+
+  //Получить толкование по слову:
+  getMeaning(word){
+    if (this._map.has(word)===false){
+      console.log("Error! Could not find entered word in dictionary (to delete)");
+      return false;
+    }
+    return this._map.get(word);
+  }
+
+}
+
 
 module.exports = {
-    fioToName,
-    filterUnique,
-    Dictionary,
-    calculateSalaryDifference
+  fioToName,
+  filterUnique,
+  Dictionary,
+  calculateSalaryDifference,
+  fooBar
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -36,11 +36,46 @@ describe('es6', () => {
         });
     });
 
+    describe('#fooBar', () => {
+        it('На n<=0 возвращает пустой массив', () => {
+            assert.deepEqual(core.fooBar(-10), []);
+        });
+
+        it('На NaN возвращает пустой массив', () => {
+            assert.deepEqual(core.fooBar("Я строка!"), []);
+        });
+
+        it('Корректрно возвращает fooBar-ы', () => {
+            assert.deepEqual(core.fooBar(16), [1, 2, "Foo", 4, "Bar", "Foo", 7, 8, "Foo", "Bar", 11, "Foo", 13, 14, "FooBar"]);
+        }); 
+    });
+
     describe('#Dictionary', () => {
-        it('экземпляр класса создается', () => {
+        it('Экземпляр класса создается', () => {
             const dic = new core.Dictionary();
 
             assert.equal(!!dic, true);
         });
+
+        it('Работает корректно', () => {
+            const dic = new core.Dictionary();
+            dic.addToDictionary('Кот','Это зверь, который мохнатый, с хвостом и мурчалом');
+            dic.addToDictionary('Пес','Это зверь, который мохнатый и с хвостом, но без мурчала');
+            dic.addToDictionary('Котлетки с макарошками','Вид пищи, который иногда ошибочно причисляется к лучшем блюдам мира');
+            dic.addToDictionary('Котлетки с пюрешкой','Вид пищи, заслуженно считающийся эталоном по своим вкусоароматическим характеристикам');
+
+            assert.deepEqual(dic.getMeaning('Абрвалг'), false);
+            assert.deepEqual(dic.getMeaning(''), false);
+            assert.deepEqual(dic.getMeaning(7), false);
+            assert.deepEqual(dic.getMeaning('Кот'), 'Это зверь, который мохнатый, с хвостом и мурчалом');
+
+            dic.changeMeaning('Кот','Можно просто котэ');
+           
+            assert.deepEqual(dic.getMeaning('Кот'), 'Можно просто котэ');
+            
+            dic.deleteWord('Кот');
+            assert.deepEqual(dic.getMeaning('Кот'), false);
+        });
+       
     });
 });
