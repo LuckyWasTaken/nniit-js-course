acc455e97b7e977b23dbe6d6aaae5f7f6bcebfe4
DanilaPetelin <danilapetelin@gmail.com>
2019-04-09 12:33:12 +0300

    oop

--- oop/core.js
+++ oop/core.js
@@ -1,13 +1,36 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
+class Point {
+    constructor(x=0,y=0) {
+        this.x = x;
+        this.y = y;
+      }
+    lengthToCenter() {
+        return Math.sqrt(this.x*this.x+this.y*this.y);
+      }
+}
 
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
+class Point3D extends Point {
+    constructor(x,y,z=0) {
+        super(x,y);
+        this.z = z;
+      }
+}
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
+class Queue {
+    constructor(arr) {
+        this.arr=arr;
+      }
+      addToEnd(x){
+        this.arr.push(x);
+      };
+      getFirst(){
+        return this.arr.shift();
+      };
+}
 
 module.exports = {
     Point,
