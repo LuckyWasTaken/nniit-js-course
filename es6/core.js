c29318c6cb16be4a9a6975a5486b2c98eca861c9
DurnevDenisR <durnev.denis.01@gmail.com>
2019-04-11 16:37:43 +0300

    H/W4

--- es6/core.js
+++ es6/core.js
@@ -3,32 +3,125 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(name) 
+{
+    let [lastName, firstName,...data]=name.split(' ');
+    return '${firstName}${lastName}';
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arr)
+ {
+     let set=new Set();
+     set.forEach(number=>{set.add(number);});
+     return [...set];
+    
+ }
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arr) 
+{
+    let minSalary = arr.reduce((min, current) => 
+    {
+        return min < current ? min : current;
+    }, 
+        arr[0]);
+        
+      let maxSalary = arr.reduce((max, current) =>
+      {
+        return max > current ? max : current;
+      },
+        arr[0]);
+    
+      return maxSalary / minSalary;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) 
+{
+    let arr=[];
+
+    for (let i=1;i<n;i++)
+    {
+        if (i%3===0)
+        {
+            arr.push("Foo");
+        } else if (i%5===0)
+        {
+            arr.push("Bar");
+        } else if (i%15===0)
+        {
+            arr.push("Foobar");
+        } else 
+        {
+            arr.push(i);
+        }
+    }
+    return arr;
+       
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary
+ {
+     constructor()
+     {
+         this._map=new Map();
+     }
+     checkword(word)
+     {
+         if (word!==string)
+         {
+             return false;
+         }
+         return true;
+     }
+     addValue(word,propereties)
+     { 
+         if (this._checkword(word)===false)
+         {
+             return false;
+         }
+
+         this._map.set(word,propereties);
+
+     }
+     setValue(word,propereties)
+     {
+         this._map.set(word,propereties);
+     }
+     getValue(word)
+     {
+         return this._map.get(word);
+     }
+
+     change(word,propereties)
+     {
+         if (this._map.get(word))
+         {
+             this.setValue(word,propereties);
+             return true;
+         }
+         return false;
+     }
+     del(word)
+     {
+         return this._map.delete(word);
+     }
+ }
 
 module.exports = {
     fioToName,
