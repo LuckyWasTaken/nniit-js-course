f7264e2535d78dedcaa3e72d89213c30ce43da7a
Nikolay <44619592+nashalagin@users.noreply.github.com>
2019-04-13 11:14:06 +0300

    add es6

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,92 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName() {
+  const str = arguments[0].split(" ");
+  return str[1] + " " + str[0];
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arr) {
+  const arrU = new Set();
+  arr.forEach(arg => arrU.add(arg));
+  arr = Array.from(arrU);
+  return arr;
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arr) {
+  if (arr == !arr) {
+    return false;
+  }
+  arr.sort((a, b) => a - b);
+  return arr.pop() / arr.shift();
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+  if (typeof n != "number") {
+    return false;
+  }
+  let arr = [];
+  let buf;
+  while (n > 0) {
+    if (n % 3 == 0 || n % 5 == 0) {
+      if (n % 3 == 0) {
+        buf = "Foo";
+      }
+      if (n % 5 == 0) {
+        buf += "Bar";
+      }
+      arr.unshift(buf);
+    } else {
+      arr.unshift(n);
+    }
+    buf = "";
+    n--;
+  }
+  return arr;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
-
+class Dictionary {
+  constructor() {
+    this.dict = new Map();
+  }
+  addWord(word, translate) {
+    if (typeof word != "string" || typeof translate != "string") {
+      console.log(`TypeError`);
+      return false;
+    }
+    if (this.dict.has(word) == true) {
+      console.log("Это слово уже присутствует в словаре!");
+      return false;
+    }
+    this.dict.set(word, translate);
+    return `Слово ${word} (${translate}) записано в словарь!`;
+  }
+  getWord(word) {
+    //console.log(`${word} - ${this.dict.get(word)}`);
+    return this.dict.get(word);
+  }
+}
 module.exports = {
-    fioToName,
-    filterUnique,
-    Dictionary,
-    calculateSalaryDifference
+  fooBar,
+  fioToName,
+  filterUnique,
+  Dictionary,
+  calculateSalaryDifference
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -1,46 +1,93 @@
-const assert = require('assert');
-const core = require('./core');
+const assert = require("assert");
+const core = require("./core");
 
-describe('es6', () => {
-    describe('#fioToName', () => {
-        it('ФИО в Имя Фамилия корректно', () => {
-            assert.equal(core.fioToName('Иванов Иван Иванович'), 'Иван Иванов');
-        });
-
-        it('ФИ в Имя Фамилия', () => {
-            assert.equal(core.fioToName('Петров Петр'), 'Петр Петров');
-        });
+describe("es6", () => {
+  describe("#fioToName", () => {
+    it("ФИО в Имя Фамилия корректно", () => {
+      assert.equal(core.fioToName("Иванов Иван Иванович"), "Иван Иванов");
     });
 
-    describe('#filterUnique', () => {
-        it('массив с уникальными равен сам себе', () => {
-            assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
-        });
+    it("ФИ в Имя Фамилия", () => {
+      assert.equal(core.fioToName("Петров Петр"), "Петр Петров");
+    });
+  });
 
-        it('массив с неуникальными отфильтрован', () => {
-            assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
-        });
-
-        it('пустой массив', () => {
-            assert.deepEqual(core.filterUnique([]), []);
-        });
+  describe("#filterUnique", () => {
+    it("массив с уникальными равен сам себе", () => {
+      assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
     });
 
-    describe('#calculateSalaryDifference', () => {
-        it('считает разницу корректно', () => {
-            assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
-        });
-
-        it('на пустой массив возвращается falsy значение', () => {
-            assert.equal(!!core.calculateSalaryDifference([]), false);
-        });
+    it("массив с неуникальными отфильтрован", () => {
+      assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
     });
 
-    describe('#Dictionary', () => {
-        it('экземпляр класса создается', () => {
-            const dic = new core.Dictionary();
-
-            assert.equal(!!dic, true);
-        });
+    it("пустой массив", () => {
+      assert.deepEqual(core.filterUnique([]), []);
     });
+  });
+
+  describe("#calculateSalaryDifference", () => {
+    it("считает разницу корректно", () => {
+      assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
+    });
+
+    it("на пустой массив возвращается falsy значение", () => {
+      assert.equal(!!core.calculateSalaryDifference([]), false);
+    });
+  });
+
+  describe("#Dictionary", () => {
+    it("экземпляр класса создается", () => {
+      const dic = new core.Dictionary();
+
+      assert.equal(!!dic, true);
+    });
+    it("Получение слова из словаря", () => {
+      const dic = new core.Dictionary();
+      dic.addWord("word", "Слово");
+      assert.equal(dic.getWord("word"), "Слово");
+    });
+    it("Добавление в словарь слова без перевода", () => {
+      const dic = new core.Dictionary();
+      assert.equal(dic.addWord("word"), false);
+    });
+    it("Повторное добавление слова в словарь", () => {
+      const dic = new core.Dictionary();
+      dic.addWord("word", "Слово");
+      assert.equal(dic.addWord("word", "Другое слово"), false);
+    });
+  });
+  describe("#fooBar", () => {
+    it("n = 2", () => {
+      assert.deepEqual(core.fooBar(2), [1, 2]);
+    });
+    it("n = 3", () => {
+      assert.deepStrictEqual(core.fooBar(3), [1, 2, "Foo"]);
+    });
+    it("n = 5", () => {
+      assert.deepStrictEqual(core.fooBar(5), [1, 2, "Foo", 4, "Bar"]);
+    });
+    it("n = 15", () => {
+      assert.deepStrictEqual(core.fooBar(15), [
+        1,
+        2,
+        "Foo",
+        4,
+        "Bar",
+        "Foo",
+        7,
+        8,
+        "Foo",
+        "Bar",
+        11,
+        "Foo",
+        13,
+        14,
+        "FooBar"
+      ]);
+    });
+    it("Обращение к функции без значения", () => {
+      assert.equal(core.fooBar(), false);
+    });
+  });
 });
