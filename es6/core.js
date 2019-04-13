f6b102b00dfac8de49c567c543828206250a4978
Alex Sadikov <sav1982@inbox.ru>
2019-04-13 22:02:11 +0300

    Sadikov v1

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,106 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(fio) {
+  let [lastName, firstName, secondName] = fio.split(" ");
+  return firstName + " " + lastName;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(buf) {
+  let set = new Set(buf);
+  let mas = [];
+  let i = 0;
+  for (let item of set) {
+    mas[i] = item;
+    i++;
+  }
+  return mas;
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(buf) {
+  if (buf.length == 0) return false;
+  else {
+    let max = buf.reduce(function(m, cur) {
+      if (m > cur) return m;
+      else return cur;
+    });
+    let min = buf.reduce(function(m, cur) {
+      if (m < cur) return m;
+      else return cur;
+    });
+    return max / min;
+  }
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+  let buf = [];
+  for (let i = 0, j=1; i < n; i++, j++) {
+    if (j % 15 == 0) buf[i] = "FooBar";
+    else if (j % 5 == 0) buf[i] = "Bar";
+    else if (j % 3 === 0) buf[i] = "Foo";
+    else buf[i] = j;
+  }
+  return buf;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+  constructor() {
+    this._dict = new Map([
+      ["Горилла", "Большая волосатая обезьяна"],
+      ["Дед Мороз", "Бородатый волшебник"]
+    ]);
+  }
+  set(word, value) {
+    this._dict.set(word, value);
+    alert(`Слово ${word} добавлено!`);
+  }
+  get(word) {
+    if (this._dict.has(word)) {
+      alert(`${word} : ${this._dict.get(word)}`);
+    } else {
+      alert("Такого слова нет в словаре!");
+    }
+  }
+  del(word) {
+    if (this._dict.has(word)) {
+      this._dict.delete(word);
+      alert(`Слово ${word} удалено!`);
+    } else {
+      alert("Такого слова нет в словаре!");
+    }
+  }
+  change(word, value) {
+    if (this._dict.has(word)) {
+      this._dict.delete(word);
+      this._dict.set(word, value);
+      alert(`${word} : ${this._dict.get(word)}`);
+    } else {
+      alert("Такого слова нет в словаре!");
+    }
+  }
+}
 
 module.exports = {
-    fioToName,
-    filterUnique,
-    Dictionary,
-    calculateSalaryDifference
+  fioToName,
+  filterUnique,
+  Dictionary,
+  calculateSalaryDifference,
+  fooBar
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -36,6 +36,20 @@ describe('es6', () => {
         });
     });
 
+    describe('#fooBar', () => {
+        it('массив из 15 элементов', () => {
+            assert.deepEqual(core.fooBar(15), [1,2,"Foo",4,"Bar","Foo",7,8,"Foo","Bar",11,"Foo",13,14,"FooBar"]);
+        });
+
+        it('массив из одного элементов', () => {
+            assert.deepEqual(core.fooBar(1), [1]);
+        });
+
+        it('массив из 3 элементов', () => {
+            assert.deepEqual(core.fooBar(3), [1,2,"Foo"]);
+        });
+    });
+
     describe('#Dictionary', () => {
         it('экземпляр класса создается', () => {
             const dic = new core.Dictionary();
