7fe748dfbe5c8f6d03dcd6e90415601b141ef424
Alexandra Ligacheva <alexandra-gall@mail.ru>
2019-04-14 21:48:13 +0300

    Add es6

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,106 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(str) {
+  let [lastName, firstName, ...rest] = str.split(" ");
+  return `${firstName} ${lastName}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arr) {
+  let set = new Set();
+  arr.forEach(arr => set.add(arr));
+  return Array.from(set);
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arr) {
+  if (arr.length == 0) {
+    return false;
+  }
+  const min = arr.reduce((previousValue, currentValue) =>
+    previousValue < currentValue ? previousValue : currentValue
+  );
+  const max = arr.reduce((previousValue, currentValue) =>
+    previousValue > currentValue ? previousValue : currentValue
+  );
+  return max / min;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+  let arr = [];
+  for (let i = 1; i < n; i++) {
+    if (i % 15 === 0) {
+      arr.push("FooBar");
+    } else if (i % 5 === 0) {
+      arr.push("Bar");
+    } else if (i % 3 === 0) {
+      arr.push("Foo");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+  constructor() {
+    this.map = new Map();
+  }
+
+  addNewWord(word, meaning) {
+    if (typeof word != "string" || typeof meaning != "string") {
+      console.log(`Error! Incorrect type`);
+      return false;
+    }
+
+    if (this.map.has(word)) {
+      console.log(`Word ${word} is already in the dictionary!`);
+      return false;
+    }
+
+    this.map.set(word, meaning);
+    console.log(`Word ${word} added to the dictionary!`);
+    return true;
+  }
+
+  findWord(word) {
+    if (this.map.has(word)) {
+      console.log(`Word ${word} means ${this.map.get(word)}`);
+      return true;
+    }
+    console.log(`Word ${word} is not in the dictionary!`);
+    return false;
+  }
+
+  deleteWord(word) {
+    if (this.map.delete(word)) {
+      console.log(`Word ${word} deleted from the dictionary`);
+      return true;
+    }
+    console.log(`Word ${word} cannot be deleted, it's not in the dictionary`);
+    return false;
+  }
+}
 
 module.exports = {
-    fioToName,
-    filterUnique,
-    Dictionary,
-    calculateSalaryDifference
+  fioToName,
+  filterUnique,
+  Dictionary,
+  calculateSalaryDifference
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -1,46 +1,86 @@
-const assert = require('assert');
-const core = require('./core');
+const assert = require("assert");
+const core = require("./core");
 
-describe('es6', () => {
-    describe('#fioToName', () => {
-        it('ФИО в Имя Фамилия корректно', () => {
-            assert.equal(core.fioToName('Иванов Иван Иванович'), 'Иван Иванов');
-        });
-
-        it('ФИ в Имя Фамилия', () => {
-            assert.equal(core.fioToName('Петров Петр'), 'Петр Петров');
-        });
+describe("es6", () => {
+  describe("#fioToName", () => {
+    it("ФИО в Имя Фамилия корректно", () => {
+      assert.equal(core.fioToName("Иванов Иван Иванович"), "Иван Иванов");
     });
 
-    describe('#filterUnique', () => {
-        it('массив с уникальными равен сам себе', () => {
-            assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
-        });
+    it("ФИ в Имя Фамилия", () => {
+      assert.equal(core.fioToName("Петров Петр"), "Петр Петров");
+    });
+  });
 
-        it('массив с неуникальными отфильтрован', () => {
-            assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
-        });
-
-        it('пустой массив', () => {
-            assert.deepEqual(core.filterUnique([]), []);
-        });
+  describe("#filterUnique", () => {
+    it("массив с уникальными равен сам себе", () => {
+      assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
     });
 
-    describe('#calculateSalaryDifference', () => {
-        it('считает разницу корректно', () => {
-            assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
-        });
-
-        it('на пустой массив возвращается falsy значение', () => {
-            assert.equal(!!core.calculateSalaryDifference([]), false);
-        });
+    it("массив с неуникальными отфильтрован", () => {
+      assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
     });
 
-    describe('#Dictionary', () => {
-        it('экземпляр класса создается', () => {
-            const dic = new core.Dictionary();
-
-            assert.equal(!!dic, true);
-        });
+    it("пустой массив", () => {
+      assert.deepEqual(core.filterUnique([]), []);
     });
+  });
+
+  describe("#calculateSalaryDifference", () => {
+    it("считает разницу корректно", () => {
+      assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
+    });
+
+    it("на пустой массив возвращается falsy значение", () => {
+      assert.equal(!!core.calculateSalaryDifference([]), false);
+    });
+  });
+
+  describe("#Dictionary", () => {
+    it("экземпляр класса создается", () => {
+      const dic = new core.Dictionary();
+      assert.equal(!!dic, true);
+    });
+
+    it("Не добавляется слово с типом Число", () => {
+      const dic = new core.Dictionary();
+      assert.equal(dic.addNewWord(123, 15), false);
+    });
+
+    it("Новое слово добавлено в словарь", () => {
+      const dic = new core.Dictionary();
+      assert.equal(dic.addNewWord("Road", "Дорога"), true);
+    });
+
+    it("Слово, которое уже есть в словаре, повторно не добавляется", () => {
+      const dic = new core.Dictionary();
+      dic.addNewWord("Road", "Дорога");
+      assert.equal(dic.addNewWord("Road", "Дорога"), false);
+    });
+
+    it("Добавленное в словарь слово найдено", () => {
+      const dic = new core.Dictionary();
+      dic.addNewWord("Road", "Дорога");
+      assert.equal(dic.findWord("Road"), true);
+    });
+
+    it("Слово в словаре не найдено (не было добавлено ранее)", () => {
+      const dic = new core.Dictionary();
+      dic.addNewWord("Road", "Дорога");
+      assert.equal(dic.findWord("Way"), false);
+    });
+
+    it("Слово, добавленное в словарь ранее, успешно удалено", () => {
+      const dic = new core.Dictionary();
+      dic.addNewWord("Road", "Дорога");
+
+      assert.equal(dic.deleteWord("Road"), true);
+    });
+
+    it("Слово, которое не было добавлено в словарь ранее, не может быть удалено", () => {
+      const dic = new core.Dictionary();
+      dic.addNewWord("Road", "Дорога");
+      assert.equal(dic.deleteWord("Way"), false);
+    });
+  });
 });
