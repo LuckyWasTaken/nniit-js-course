8cb09e84c8db49792990ad0a9d76342635481405
Anastasia Spiridonova <asb_gzas@mail.ru>
2019-04-16 19:02:11 +0300

    js structures (with changes) and closures

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,17 @@
-// Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
-function sum(a) {
+function sum (a){
+    return function (b){
+        return a + b;
+    }
 }
 
-// Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+    let currentCount = 0;
+    let currentSum = 0;
+    return function(a) {
+        currentCount++;
+        currentSum += a;
+        return currentSum/currentCount;
+    }
 }
 
 module.exports = {
