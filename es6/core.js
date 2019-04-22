683fa3c3942fb131ffc80c234ebc6c44fa480818
Anastasia Spiridonova <asb_gzas@mail.ru>
2019-04-22 14:55:22 +0300

    js es6 Spiridonova

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,171 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(fullName) {
+    if (typeof fullName !== "string" || fullName.split(" ").length < 2){
+        return false;
+    }
+
+    let [surname = "", name = ""] = fullName.split(" ");
+
+    return (name + " " + surname);
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arrayOfNumbers) {
+    if ((!Array.isArray(arrayOfNumbers)) || (arrayOfNumbers.some(isNaN))){
+        return null; 
+    }
+
+    return Array.from(new Set(arrayOfNumbers));
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arrayOfSalaries) {
+
+    /*function isNegative(number) {
+        return number <= 0;
+    }*/
+    let isNegative = number =>  number <= 0;
+
+    if (!Array.isArray(arrayOfSalaries) || !arrayOfSalaries.length || arrayOfSalaries.some(isNaN) || arrayOfSalaries.some(isNegative)){
+        return false;
+    }
+
+    let max = arrayOfSalaries[0];
+    let min = arrayOfSalaries[0];
+
+    min = arrayOfSalaries.reduce(function(min, item){
+        if (min > item){
+            return item;
+        }
+        else {
+            return min;
+        }
+    })
+
+    max = arrayOfSalaries.reduce(function(max, item){
+        if (max < item){
+            return item;
+        }
+        else {
+            return max;
+        }
+    })
+
+    return max / min;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+    if ((isNaN(n)) || (n <=0)){
+        return false;
+    }
+
+    let arr = [];
+
+    for (i = 0; i < n; i++){
+        if  (0 === ((i+1) % 15)){
+              arr[i] = "FooBar";
+        }
+         else if (0 === ((i+1) % 3)){
+            arr[i] = "Foo";
+        }
+        else if (0 === ((i+1) % 5)){
+            arr[i] = "Bar";
+        }
+        else {
+            arr[i] = i + 1;
+        }
+    }
+
+    return arr;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+// Добавить слово и его толкование в словарь
+// Изменить толкование слова
+// Удалить слово из словаря
+// Получить толкование по слову
+class Dictionary {
+    constructor() {
+		this.engToRus = new Map;
+    }
+
+    addWord(wordEng, wordRus){
+        if (typeof wordEng !== "string" || typeof wordRus !== "string" || 0 === wordEng.length || 0 === wordRus.length){
+            console.log("Wrong input");
+            return false;
+        }
+
+        if (!this.engToRus.has(wordEng)){
+            this.engToRus.set(wordEng, wordRus);
+            console.log("The word " + wordEng + " was added");
+        }
+        else {
+            console.log("The word " + wordEng + " was found in the dictionary");
+        }
+    }
+
+    changeTranslation(wordEng, wordRus){
+        if (typeof wordEng !== "string" || typeof wordRus !== "string" || 0 === wordEng.length || 0 === wordRus.length){
+            console.log("Wrong input");
+            return false;
+        }
+
+        if (this.engToRus.delete(wordEng)){
+            this.engToRus.set(wordEng, wordRus);
+            console.log("The translation of the word " + wordEng + " was changed");
+        }
+        else{
+            this.engToRus.set(wordEng, wordRus);
+            console.log("The word " + wordEng + " was added");
+        }
+    }
+
+    deleteWord(wordEng){
+        if (typeof wordEng !== "string" || 0 === wordEng.length){
+            console.log("Wrong input");
+            return false;
+        }
+            
+        if (this.engToRus.delete(wordEng)){
+            console.log("The word " + wordEng + " was deleted");
+        };
+    }
+
+    getTranslation(wordEng){
+        if (typeof wordEng !== "string" || 0 === wordEng.length){
+            console.log("Wrong input");
+            return false;
+        }
+    
+        if (!this.engToRus.has(wordEng)){
+            console.log("There is no such word in the dictionary");
+            return false;
+        }
+
+        return this.engToRus.get(wordEng);
+    }
+}
 
 module.exports = {
     fioToName,
     filterUnique,
     Dictionary,
-    calculateSalaryDifference
+    calculateSalaryDifference,
+    fooBar
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -31,16 +31,120 @@ describe('es6', () => {
             assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
         });
 
-        it('на пустой массив возвращается falsy значение', () => {
+        it('на пустой массив возвращается false значение', () => {
             assert.equal(!!core.calculateSalaryDifference([]), false);
         });
     });
 
+    describe('#fooBar', () => {
+        it('выдает корректный массив', () => {
+            assert.deepEqual(core.fooBar(17), [1, 2, "Foo", 4, "Bar", "Foo", 7, 8, "Foo", "Bar", 11, "Foo", 13, 14, "FooBar", 16, 17]);
+        });
+
+        it('на параметр меньше 1 выдает false значение', () => {
+            assert.equal(!!core.fooBar(-1), false);
+        });
+
+        it('на параметр 0 выдает false значение', () => {
+            assert.equal(!!core.fooBar(0), false);
+        });
+
+        it('на нечисловой параметр выдает false значение', () => {
+            assert.equal(!!core.fooBar("вася"), false);
+        });
+    });
+
     describe('#Dictionary', () => {
+        const dic = new core.Dictionary();
         it('экземпляр класса создается', () => {
-            const dic = new core.Dictionary();
+           // const dic = new core.Dictionary();
 
             assert.equal(!!dic, true);
+        });
+
+        describe('#addWord', () => {
+
+            it('на нестроковый параметр в key выдает false значение', () => {
+                assert.equal(!!dic.addWord(5, "d"), false);
+            });
+
+            it('на нестроковый параметр в value выдает false значение', () => {
+                assert.equal(!!dic.addWord("d", 7), false);
+            });
+
+            it('на пустую строку в параметре выдает false значение', () => {
+                assert.equal(!!dic.addWord("", ""), false);
+            });
+
+            it('корректно добавляет слово', () => {
+                dic.addWord("time", "время");
+                assert.equal(!!dic.engToRus.has("time"), true);
+                assert.equal(dic.getTranslation("time"), "время");
+            });
+        });
+
+        describe('#changeTranslation', () => {
+            it('на нестроковый параметр в key выдает false значение', () => {
+                assert.equal(!!dic.changeTranslation(5, "d"), false);
+            });
+
+            it('на нестроковый параметр в value выдает false значение', () => {
+                assert.equal(!!dic.changeTranslation("d", 7), false);
+            });
+
+            it('на пустую строку в параметре выдает false значение', () => {
+                assert.equal(!!dic.changeTranslation("", ""), false);
+            });
+
+            it('корректно меняет перевод', () => {
+                dic.addWord("room", "пространство");
+                dic.changeTranslation("room", "комната, пространство");
+                assert.equal(!!dic.engToRus.has("room"), true);
+                assert.equal(dic.getTranslation("room"), "комната, пространство");
+            });
+
+            it('добавляет перевод, если слова не было в словаре', () => {
+                dic.changeTranslation("I", "я");
+                assert.equal(!!dic.engToRus.has("I"), true);
+                assert.equal(dic.getTranslation("I"), "я");
+            });
+        });
+
+        describe('#deleteWord', () => {
+            it('на нестроковый параметр в key выдает false значение', () => {
+                assert.equal(!!dic.deleteWord(5), false);
+            });
+
+
+            it('на пустую строку в параметре выдает false значение', () => {
+                assert.equal(!!dic.deleteWord(""), false);
+            });
+
+            it('корректно удаляет слово', () => {
+                dic.addWord("he", "он");
+                dic.deleteWord("he");
+                assert.equal(!!dic.engToRus.has("he"), false);
+            });
+        });
+
+        describe('#getTranslation', () => {
+            it('на нестроковый параметр в key выдает false значение', () => {
+                assert.equal(!!dic.getTranslation(5), false);
+            });
+
+
+            it('на пустую строку в параметре выдает false значение', () => {
+                assert.equal(!!dic.getTranslation(""), false);
+            });
+
+            it('корректно возвращает перевод слова', () => {
+                dic.addWord("she", "она");
+                assert.equal(dic.getTranslation("she"), "она");
+            });
+
+            it('если слова нет возвращает false значение', () => {
+                assert.equal(!!dic.getTranslation("we"), false);
+            });
         });
     });
 });
