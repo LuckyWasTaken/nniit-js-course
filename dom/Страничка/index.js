ff2b3a7aa8ed64e94e917126af3a358c401f3a98
DurnevDenisR <44579103+DurnevDenisR@users.noreply.github.com>
2019-04-25 10:46:09 +0300

    lesson5

---
+++ dom/Страничка/index.js
@@ -0,0 +1,12 @@
+const textColor=document.getElementById("text");
+
+
+function changeColorBlue()
+{
+    textColor.style.color = "blue";
+}
+
+function changeColorRed()
+{
+    textColor.style.color = "red";
+}
