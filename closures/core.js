670b212ce37ab919867d5b77c08f314f91d4788d
Максим Захаров <mttpr28@outlook.com>
2019-04-28 12:17:58 +0300

    Update core.js

--- closures/core.js
+++ closures/core.js
@@ -1,12 +1,39 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
 function sum(a) {
+  return function (b) 
+  {
+      return a + b;  
+  }
 }
 
+
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
-function average() {
+function average() 
+{
+  let avgValue = [];
+  avgValue.push(0);
+  let inputValue = arguments[0];
+  return function(inputValue)
+  {
+      if (avgValue[0] == 0)
+      {
+        avgValue[0] = inputValue;
+        return inputValue;
+      }
+      else
+      { 
+        avgValue.push(inputValue);
+        currentAvgValue = 0;
+        for (var i=0; i<avgValue.length; i++)
+        {
+          currentAvgValue = currentAvgValue + avgValue[i];
+        } 
+        return currentAvgValue / avgValue.length;
+      }
+  }
 }
 
 module.exports = {
   sum,
   average
-};
+}
