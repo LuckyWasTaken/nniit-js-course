3bdc91f0b1687117d3971f2a76ddd0d6f01345d1
BlackLunx <46784716+BlackLunx@users.noreply.github.com>
2019-04-28 15:28:33 +0300

    Update core.js

--- basics/core.js
+++ basics/core.js
@@ -1,29 +1,50 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    return ((n & n) == n);
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let arr = [];
+    for(let i = 2;i <= 20;i++){
+        if(i % 2 == 0) arr.push(i)
+    }
+    return arr;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    let sum = 0;
+    for(let i = 1;i <= n;i++){
+        sum += i;
+    }
+    return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if(n == 0) return 0;
+    return (n + recSumTo(n - 1));
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    if(n == 1) return 1;
+    return factorial(n - 1) * n;
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    if(n == 0) return false;
+    return ((n & (n - 1)) == 0);
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    if(n == 2) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = {
