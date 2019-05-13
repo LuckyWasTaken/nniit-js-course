285ccd2e8e5bf6594c2b9bdf4249fd4e4d37054a
Alexandra Ligacheva <alexandra-gall@mail.ru>
2019-05-13 23:15:39 +0300

    Add mixed homework

---
+++ mixed/task/index.js
@@ -0,0 +1,35 @@
+function loadInformation() {
+  const userID = parseInt(document.getElementById("userID").value);
+  let totalresult;
+  console.log(userID);
+
+  fetch("https://jsonplaceholder.typicode.com/todos/")
+    .then(response => response.json())
+    .then(json => {
+      totalresult = json.filter(object => object["userId"] === userID);
+      console.log("parsed");
+      showResult(totalresult);
+    })
+    .catch(function(ex) {
+      console.log("parsing failed", ex);
+    });
+}
+
+function showResult(totalresult) {
+  console.log(totalresult.length);
+  document.getElementById("list").innerHTML = "";
+  if (totalresult.length === 0) {
+    const str = document.createElement("p");
+    str.innerText = "You entered incorrect ID";
+    list.appendChild(str);
+  }
+  totalresult.forEach(element => {
+    const num = document.createElement("li");
+    list.appendChild(num);
+    for (let key in element) {
+      const el = document.createElement("p");
+      el.innerHTML = `${key}: ${element[key]}`;
+      list.appendChild(el);
+    }
+  });
+}
