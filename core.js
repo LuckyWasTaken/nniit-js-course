76df6bfc0d855c679793b241b0d63580419f37b0
DmitriKonnovNN <44975416+DmitriKonnovNN@users.noreply.github.com>
2019-05-16 16:57:44 +0300

    Lab 3

--- core.js
+++ core.js
@@ -1,38 +1,58 @@
-// Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
-// Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {
-    constructor (x = 0, y = 0){
-        this.x = x;
-        this.y + y;
-    };
-    get distanceToCenter () {
-        return `the current distance to the center is ${Math.sqrt(this.x * this.x + this.y*this.y)}`;
-    };
+//Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
+function capitalize(str) {
+  if (typeof str !== 'string') {return null};
+  return str.replace(/(?:^|\s)\S/g, function(a){ return a.toUpperCase(); } );
 }
 
-// Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
-// который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {
-    constructor (x,y,z = 0){super(x,y);
-        this.z = z;
-    }
+//Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
+function truncate(str, n) {
+  if (typeof str !== 'string' || typeof n !== 'number') {return null} // comparing type of input  with returned str-value of F typeof
+  if (str.length < n) return str;
+  return str = str.substring (0, n) + "..."; // getting string cut from 0- to N-elements 
 }
 
-// Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {
-    constructor (){
-        this.currentQueue = [];
-    };
-    setUnitBack (x) {
-        this.currentQueue.push(x);
-    };
-    getUnitForward (){ return this.currentQueue.shift();
-
-    };
+//Определите, пуст ли объект
+function isEmpty(obj) {  
+  if (!obj) {
+  throw new Error ('ERROR! The current object is empty'); // throwing an error
 }
+else return true;}
+
+//Напишите функцию, умножающую численные свойства на 2
+function multiply(obj) {
+  if (!obj) {return null;};
+  for (let key in obj) {
+    obj[key] *= 2;
+  }
+  return obj;
+}
+
+//Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
+function sumArr(arr) {
+  const initialValue = 0;
+  return arr.reduce(function(accumulator, currentValue) {return accumulator + currentValue;},initialValue);
+}
+
+//Напишите функцию, определяющую, является ли данное слово палиндромом
+function isPali(str) {if (str == "") {return true};
+for (let i = 0, j = str.length - 1; i < str.length; i++, j--){
+  if (str[i] !=  str[j]) return false;
+  else return true;};
+    /* in the manner of JS:
+  function isPali(str) {
+  let arr = str.split ("");
+  arr = arr.reverse();
+  arr = arr.join(""); 
+  if (arr === str) {return true}
+  else return false;}
+  //ну или как-то так.//
+*/}
 
 module.exports = {
-    Point,
-    Point3D,
-    Queue,
+  capitalize,
+  truncate,
+  isEmpty,
+  multiply,
+  sumArr,
+  isPali
 };
