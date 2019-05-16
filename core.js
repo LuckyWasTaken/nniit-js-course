bc33f0596633c6ac26fe0996653a72bbd7fb7b35
DmitriKonnovNN <44975416+DmitriKonnovNN@users.noreply.github.com>
2019-05-16 16:59:08 +0300

    Lab 2
    I've been facing some debugging problems, ain't I.((

--- core.js
+++ core.js
@@ -1,58 +1,24 @@
-//Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {
-  if (typeof str !== 'string') {return null};
-  return str.replace(/(?:^|\s)\S/g, function(a){ return a.toUpperCase(); } );
-}
-
-//Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {
-  if (typeof str !== 'string' || typeof n !== 'number') {return null} // comparing type of input  with returned str-value of F typeof
-  if (str.length < n) return str;
-  return str = str.substring (0, n) + "..."; // getting string cut from 0- to N-elements 
-}
-
-//Определите, пуст ли объект
-function isEmpty(obj) {  
-  if (!obj) {
-  throw new Error ('ERROR! The current object is empty'); // throwing an error
-}
-else return true;}
-
-//Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {
-  if (!obj) {return null;};
-  for (let key in obj) {
-    obj[key] *= 2;
+// Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
+function sum(a) {
+  let sumBuffer = a;
+  function getSum (b){
+    sumBuffer +=b;
+    return getSum;
   }
-  return obj;
+  getSum.toString = function(){return sumBuffer;};
+  return getSum; // code successfully lifted :( just a+b ain't tough enough
 }
 
-//Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {
-  const initialValue = 0;
-  return arr.reduce(function(accumulator, currentValue) {return accumulator + currentValue;},initialValue);
+// Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
+function average() {
+  let buffer = [], sumToDevide = 0; // instead, a counter can be used too that's to be put into the function (a)...
+  return function (a){
+    buffer.push(a);
+    sumToDevide += a;
+    return sumToDevide/buffer.length; // or divided by counter
 }
-
-//Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {if (str == "") {return true};
-for (let i = 0, j = str.length - 1; i < str.length; i++, j--){
-  if (str[i] !=  str[j]) return false;
-  else return true;};
-    /* in the manner of JS:
-  function isPali(str) {
-  let arr = str.split ("");
-  arr = arr.reverse();
-  arr = arr.join(""); 
-  if (arr === str) {return true}
-  else return false;}
-  //ну или как-то так.//
-*/}
 
 module.exports = {
-  capitalize,
-  truncate,
-  isEmpty,
-  multiply,
-  sumArr,
-  isPali
+  sum,
+  average
 };
