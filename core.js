7ee3430463422a2da4cadce98f0c5f70028a941f
DmitriKonnovNN <44975416+DmitriKonnovNN@users.noreply.github.com>
2019-05-18 20:57:40 +0300

    Add files via upload

--- core.js
+++ core.js
@@ -1,78 +1,42 @@
-//Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
-function isInteger(n) {
-    if ( ~~n == n );
+// Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
+// Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
+class Point {
+    constructor (x = 0, y = 0){
+        this.x = x;
+        this.y = y;
+    };
+    get distanceToCenter () {
+        return Math.sqrt(Math.pow(this.x,2) + Math.pow(this.y,2));
+    };
 }
 
-//Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
-function even() {   
-    let BasicCharacter = 2, FinishCharacter = 20;
-    let ArrayOfEvenNumbers = [];
-    for (let i = 0; i < BasicCharacter/FinishCharacter; i++)
-    {
-        ArrayOfEvenNumbers [i] = BasicCharacter;
-        BasicCharacter+2;
+// Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
+// который будет наследоваться от точки в двумерном пространстве
+class Point3D extends Point {
+    constructor (x,y,z = 0){super(x,y);
+        this.z = z;
     }
-     return console.log (ArrayOfEvenNumbers);
+    get distanceToCenter (){
+        return Math.sqrt(Math.pow(super.distanceToCenter,2)+Math.pow(this.z,2));
+    }
+
 }
 
-//Напишите функцию, считающую сумму чисел до заданного используя цикл
-function sumTo(n) {
-    let sum = 0;
-    for (; n > 0; n--)
-    {
-       sum += n;
-    }
-    return sum;
-}
+// Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
+class Queue {
+    constructor (){
+        this._currentQueue = [];
+    };
+    setUnitBack (x) {
+        this._currentQueue.push(x);
+    };
+    getUnitForward (){ return this._currentQueue.shift();
 
-//Напишите функцию, считающую сумму чисел до заданного используя рекурсию
-function recSumTo(n) {
-    if (n > 0)
-    {
-        if (n == 1)
-        {
-            return 1;
-        }
-        else return n + (recSumTo - 1);
-
-    }
-    else return console.log ("Your number doesn't fit conditions. Do not try it again you retard!")
-}
-
-//Напишите функцию, считающую факториал заданного числа
-function factorial(n) {
-    if (n > 0 && n < 66 && ~~n == n) // I considered the restriction "66" needed cuz otherwise the factorial's gonna be larger than unsigned long long int.
-    {
-    if (n == 1)
-    {
-        return 1;
-    }
-    else 
-    {
-        return n * factorial (n - 1);
-    }
-     } else throw new Error ("You number doesn't fit conditions!");
-}
-
-
-//Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
-function isBinary(n) {
-    if (n == 2 || 1) return true;
-    else if (n == 0) return false;
-    else if (n % 2 == 0)
-    {
-        isBinary (n / 2);
-    }
-    else return false;
-}
-
-//Напишите функцию, которая находит N-е число Фибоначчи
-function fibonacci(n) {
-    if (n <= 1)
-    return n;
-    else return fibonacci (n-1) + fibonacci (n - 2);
+    };
 }
 
 module.exports = {
-    isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
-}
+    Point,
+    Point3D,
+    Queue,
+};
