822401224121914a13d9f22487ac4447e6a228ff
geniusserg <50704370+geniusserg@users.noreply.github.com>
2019-05-18 22:28:00 +0300

    Update core.js

--- basics/core.js
+++ basics/core.js
@@ -5,32 +5,40 @@ function isInteger(n) {
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
-	for(var a = [], i = 0; i < 10; a[i] = (i+1)*2, i++){}
-	return a;
+	var array = [];
+	for(var i = 0; i < 10; i++){
+		array[i] = (i+1)*2;
+	}
+	return array;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
-	for (var a = 0, i = 1; i <= n; a += i, i++){}
-	return a;
+	for (var sum = 0, i = 1; i <= n; sum += i, i++){}
+	return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
-	var a = n;
-	if(n!=0){a += recSumTo(n-1);} 
-	return a;
+	if (n !== 0){
+		return n+recSumTo(n-1);
+	}
+	return 0;
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
-	for (var a = 1, i = 1; i <= n; a *= i, i++){}
-	return a;
+	for (var intResult = 1, i = 1; i <= n; intResult *= i, i++){}
+	return intResult;
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
-	for(var a = 2; a <= n; a *= 2){if (a == n){return true;}}
+	for(var intCurrent = 2; intCurrent <= n; intCurrent *= 2){
+		if (intCurrent == n){
+			return true;
+		}
+	}
 	return false;
 }
 
@@ -42,9 +50,7 @@ function fibonacci(n) {
 	if(n==2){
 		return 1;
 	}
-	if (n>2){
-		return fibonacci(n-1)+fibonacci(n-2);
-	}
+	return fibonacci(n-1)+fibonacci(n-2);
 }
 
 module.exports = {
