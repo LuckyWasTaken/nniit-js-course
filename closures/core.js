2ef532458796b898ebe9ec29207052bfaf29c164
mp913 <zarkov01@mail.ru>
2019-05-21 18:47:47 +0300

    Выполнил дз из папки closures

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,32 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
 function sum(a) {
+    let currentSum = a;
+    
+    function plus(b){
+        currentSum += b;
+        return plus;
+    }
+    
+    plus.toString = function() {
+        return currentSum;
+    };
+    
+    return plus;
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+  let counter = 0;
+  let sum = 0;
+
+  function addANumber(a) {
+    sum += a;
+    counter++;
+    let ans = sum / counter;
+    return ans;
+  };
+  
+  return addANumber;
 }
 
 module.exports = {
