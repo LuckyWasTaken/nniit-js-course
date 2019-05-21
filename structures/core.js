c6b0e3dff221016d21a0f5074696f8625f92f9cf
mp913 <zarkov01@mail.ru>
2019-05-21 17:43:11 +0300

    Выполнил домашнее задание в папке structures

--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,68 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+    let flag = 1;
+    for (let i =0; i< str.length; i++)
+    {
+        if (str[i] == ' ')
+        {
+            flag = 1;
+            continue;
+        }
+        if (flag == 1)
+        {
+            str = str.substr(0, i) + str[i].toUpperCase()+ str.substr(i+1);
+            flag = 0;
+        }
+    }
+    return str;
+    }
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+    if (n>str.length)
+    {
+        return str;
+    }
+    str = str.substr(0, n);
+    str += '...'
+    return str;
+    }
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+    for (let key in obj) {
+        if (obj[key]*2 == obj[key]*2)
+        {
+            obj[key] = obj[key] * 2;
+        }
+    }
+    return obj;
+    }
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+    let sum = arr.reduce(function(sum, current) {return sum + current;});
+    return sum;
+    }
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+    for (let i =0; i<str.length; i++){
+        if (str[i]!=str[str.length-1-i])
+        {
+            return false;
+        }
+    }
+    return true;
+    }
 
 module.exports = {
   capitalize,
