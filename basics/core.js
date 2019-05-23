768a56d6254d36f62f224ae202e008c5589d410c
Andrey Talanin <talanin.aa.nnov@gmail.com>
2019-05-23 17:47:48 +0300

    Implement the functions

--- basics/core.js
+++ basics/core.js
@@ -1,31 +1,85 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
-function isInteger(n) {
-}
+function isInteger(n)
+{
+    return n == (n ^ 0);
+};
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
-function even() {
-}
+function even()
+{
+    var array = [];
+    var lastIndex = 0;
+    for (var i = 2; i <= 20; i += 2)
+        array[lastIndex++] = i;
+
+    return array;
+};
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
-function sumTo(n) {
-}
+function sumTo(n)
+{
+    var result = 0;
+    for (var i = 1; i <= n; i++)
+        result += i;
+
+    return result;
+};
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
-function recSumTo(n) {
-}
+function recSumTo(n)
+{
+    if (n > 1)
+        return n + recSumTo(n - 1);
+    else
+        return 1;
+};
 
 //Напишите функцию, считающую факториал заданного числа
-function factorial(n) {
-}
+function factorial(n)
+{
+    var result = 1;
+    for (var i = 1; i <= n; i++)
+        result *= i;
+
+    return result;
+};
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
-function isBinary(n) {
-}
+function isBinary(n)
+{
+    var i = n;
+    while (i > 1)
+        if (i % 2 == 0)
+            i /= 2;
+        else
+            return false;
+
+    return true;
+};
 
 //Напишите функцию, которая находит N-е число Фибоначчи
-function fibonacci(n) {
-}
+function fibonacci(n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+    {
+        var firstResult = 0, secondResult = 1;
+        for (var i = 2; i < n; i++)
+        {
+            var result = firstResult + secondResult;
 
-module.exports = {
+            firstResult = secondResult;
+            secondResult = result;
+        }
+
+        return firstResult + secondResult;
+    }
+};
+
+module.exports =
+{
     isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
 }
