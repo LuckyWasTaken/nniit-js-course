8b67810c6030f60fb9e5c3d99d5a29c77660bd2e
NoVarlok <50706895+NoVarlok@users.noreply.github.com>
2019-05-23 19:40:56 +0300

    Яхтин Леонид(18ПИ-1) (closures)
    closures

--- core.js
+++ core.js
@@ -1,55 +1,25 @@
-//Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
-function isInteger(n) {
-    return n==(n^0);
+// Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
+function sum(a) {
+  return function sumSecond(value){
+    return a+value;
+  }
 }
 
-//Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
-function even() {
-    return [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-}
+// Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
+function average() {
+  let sum=0;
+  let cnt=0;
 
-//Напишите функцию, считающую сумму чисел до заданного используя цикл
-function sumTo(n) {
-    let sum=0;
-    for(let i=1;i<=n;i=i+1)sum+=i;
-    return sum;
-}
+  function changeAverage(value){
+    sum+=value;
+    cnt++;
+    return sum/cnt;
+  }
 
-//Напишите функцию, считающую сумму чисел до заданного используя рекурсию
-function recSumTo(n) {
-    if(n==0)return 0;
-    return n+recSumTo(n-1);
-}
-
-//Напишите функцию, считающую факториал заданного числа
-function factorial(n) {
-    if(n==0)return 1;
-    let ans=1;
-    for(let i=1;i<=n;i++)ans*=i;
-    return ans;
-}
-
-//Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
-function isBinary(n) {
-    while(n>1){
-        if(n%2==1)return false;
-        n/=2;
-    }
-    return n!=0;
-}
-
-//Напишите функцию, которая находит N-е число Фибоначчи
-function fibonacci(n) {
-    let fib1=0;
-    let fib2=1;
-    for(i=1;i<=n;i++){
-        let fib3=fib1+fib2;
-        fib1=fib2;
-        fib2=fib3;
-    }
-    return fib1;
+  return changeAverage;
 }
 
 module.exports = {
-    isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
-}
+  sum,
+  average
+};
