a1a5d260cef8347593e5c700bd83fedbeb00c178
NoVarlok <50706895+NoVarlok@users.noreply.github.com>
2019-05-23 19:43:04 +0300

    Яхтин Леонид (18ПИ-1) (structures)
    structures

--- core.js
+++ core.js
@@ -1,25 +1,55 @@
-// Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
-function sum(a) {
-  return function sumSecond(value){
-    return a+value;
+//Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
+function capitalize(str) {
+  let ans="";
+  for(let i=0;i<str.length;i++){
+    if(i===0 || str[i-1]===' '){
+      ans+=str[i].toUpperCase();
+    }
+    else ans+=str[i];
   }
+  return ans;
 }
 
-// Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
-function average() {
-  let sum=0;
-  let cnt=0;
+//Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
+function truncate(str, n) {
+  if(str.length<n)return str;
+  return str.substring(0,n)+"...";
+}
 
-  function changeAverage(value){
-    sum+=value;
-    cnt++;
-    return sum/cnt;
+//Определите, пуст ли объект
+function isEmpty(obj) {
+  for(let key in obj){
+    return false;
   }
+  return true;
+}
 
-  return changeAverage;
+//Напишите функцию, умножающую численные свойства на 2
+function multiply(obj) {
+  for(let key in obj){
+    if(isFinite(obj[key]))obj[key]*=2;
+  }
+  return obj;
+}
+
+//Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
+function sumArr(arr) {
+  return arr.reduce((sum,cur)=>sum+cur);
+}
+
+//Напишите функцию, определяющую, является ли данное слово палиндромом
+function isPali(str) {
+  for(let i=0;i<str.length/2;i++){
+    if(str[i]!==str[str.length-1-i])return false;
+  }
+  return true;
 }
 
 module.exports = {
-  sum,
-  average
+  capitalize,
+  truncate,
+  isEmpty,
+  multiply,
+  sumArr,
+  isPali
 };
