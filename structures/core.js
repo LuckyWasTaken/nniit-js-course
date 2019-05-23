e15e97b0206537c815c5c9d7a5a485dce984b083
Krylova Maria <krylova.maria.2629@gmail.com>
2019-05-23 21:16:37 +0300

    add structures

--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,48 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  str = str.split(' ');
+  for (let i = 0; i < str.length; i ++){
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].substring(1,str[i].length);
+  }
+  str = str.join(' ');
+  return str;
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+    return str.substring(0,n);
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for (let i in obj){
+    if (obj.hasOwnProperty(i)) return false;
+  }
+  return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for (let i in obj){
+    if (+obj[i]){
+      obj[i] *= 2;
+    }
+  }
+  return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  var ans = arr.reduce(function(s, i){
+    return s + i;
+  },0)
+  return ans;
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+  return str == str.split(' ').reverse().join(' ');
+}
 
 module.exports = {
   capitalize,
