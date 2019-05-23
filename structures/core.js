7bfea0aa26988cb4a933e65665cd480921d8449b
BlackLunx <46784716+BlackLunx@users.noreply.github.com>
2019-05-23 19:45:34 +0300

    Update core.js

--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,49 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  if (Boolean(!str.length)) return str;
+  return str
+    .split(" ")
+    .map(function(word) {
+      return word[0].toUpperCase() + word.substr(1);
+    })
+    .join(" ");
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+  if (str.length < n) return str;
+  return str.substr(0, n) + "...";
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for (var item in obj) return false;
+  return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for (var item in obj) {
+    if (isFinite(obj[item])) obj[item] *= 2;
+  }
+  return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  return arr.reduce(function(sum, curNumber) {
+    return sum + curNumber;
+  });
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+  let reversedStr = str
+    .split("")
+    .reverse()
+    .join("");
+  return str == reversedStr;
+}
 
 module.exports = {
   capitalize,
