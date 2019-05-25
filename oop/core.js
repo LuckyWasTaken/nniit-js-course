28081b1d9a6f3efa8b770c47bdaa84fc8540c0f2
AntonPavlovNN <44950214+AntonPavlovNN@users.noreply.github.com>
2019-05-25 23:46:04 +0300

    Lesson 3 Pavlov

--- oop/core.js
+++ oop/core.js
@@ -1,13 +1,46 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
+class Point {
+    constructor(x = 0, y = 0){
+		this.x = x;
+		this.y = y;
+	}
+
+	distance() {
+		return Math.sqrt(this.x*this.x + this.y*this.y);
+	}
+}
 
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
+class Point3D extends Point {
+    constructor(x, y, z = 0){
+		super(x, y);
+		this.z = z;
+	}
+
+	distance(){
+		return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+    }
+}
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
+class Queue {
+    constructor(){
+		this._arr = [];
+    }
+
+	addLastElem(elem){
+		this._arr.push(elem);
+	}
+
+	getFirstElem(){
+		if (this._arr.lenght === 0){
+			console.log("Очередь пустая, первого элемента нет");
+		}
+		return this._arr.shift();
+	}
+}
 
 module.exports = {
     Point,
