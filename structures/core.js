37aac659af6e08ff938fca741c50bf0cf1b05058
AntonPavlovNN <44950214+AntonPavlovNN@users.noreply.github.com>
2019-05-25 22:12:45 +0300

    Lesson 2 Pavlov

--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,45 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  if (!str) return str;
+  return str[0].toUpperCase() + str.slice(1);
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+  if (!str) return;
+  if (str.length < n) {
+    return str;
+  }
+  return `${str.substr(0, n)}...`;
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for (let key in obj) {
+    if (parseFloat(obj[key])) {
+      obj[key] *= 2;
+    }
+  }
+  return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  return arr.reduce((a, b) => a + b, 0);
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
 function isPali(str) {}
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
 
 module.exports = {
   capitalize,
