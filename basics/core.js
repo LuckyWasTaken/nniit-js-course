62d281117bf17edda7ad36b54fa6633695b04535
stifeevn <stifeev.nikita@mail.ru>
2019-05-26 18:42:32 +0500

    update basic

--- basics/core.js
+++ basics/core.js
@@ -1,29 +1,48 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    return (n ^ 0) === n;
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let evenArr = [];
+    for (let i = 2; i <= 20; i += 2) {
+        evenArr.push(i);
+    }
+    return evenArr;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if (n == 1) return 1;
+    return n + recSumTo(n - 1);
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    if ((n !== 0) && ((n & (n - 1)) === 0)) return true;
+    return false;
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = {
