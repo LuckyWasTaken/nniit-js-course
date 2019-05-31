df5be3a293526091d3105ae07023ad642d438732
stifeevn <stifeev.nikita@mail.ru>
2019-05-31 20:08:36 +0500

    Update OOP and ES6

--- es6/core.js
+++ es6/core.js
@@ -3,32 +3,75 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(fio = String.prototype) {
+    const [surname, name] = fio.split(' ');
+    return `${name} ${surname}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(array = Array.prototype) {
+    const set = new Set();
+    array.forEach((number) => set.add(number));
+    return Array.from(set);
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(array = Array.prototype) {
+    if (array.length === 0) return false;
+
+    const lastIdx = array.length - 1;
+    const { 0: min, [lastIdx]: max } = array.sort((a, b) => a - b);
+
+    return max / min;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(length = Number.prototype) {
+    return Array.from({length}).map((_, idx) => {
+            const res = idx + 1;
+
+            if (res % 15 === 0) return 'FooBar';
+            if (res % 5 === 0) return 'Bar';
+            if (res % 3 === 0) return 'Foo';
+
+            return res;
+        });
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor() {
+        this._store = new Map();
+    }
+
+    add(word, translate) {
+        if (typeof word !== 'string' || typeof translate !== 'string') return;
+        this._store.set(word, translate);
+    }
+
+    getTransplate(word) {
+        if (typeof word !== 'string') return null;
+        return this._store.get(word);
+    }
+}
+
+const dic = new Dictionary();
+
+dic.add('hello', 'привет');
+dic.add('world', 'мир');
 
 module.exports = {
     fioToName,
--- oop/core.js
+++ oop/core.js
@@ -1,16 +1,47 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
+class Point {
+    constructor(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+}
 
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
+class Point3D extends Point {
+    constructor(x, y, z) {
+        super(x, y);
+        this.z = z || 0;
+    }
+}
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
+class Queue {
+    constructor() {
+        this._store = [];
+    }
+
+    set add(value) {
+        this._store.push(value);
+    }
+
+    get get() {
+        return this._store.pop();
+    }
+}
+
+const queue = new Queue();
+queue.add = 1;
+queue.add = 2;
+queue.add = 3;
+queue.get;
+queue.get;
+queue.get;
 
 module.exports = {
     Point,
     Point3D,
     Queue,
 };
+
