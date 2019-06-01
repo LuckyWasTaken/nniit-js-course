c9aeb67c94bf86a3afc17ac5f043b7dec4c6d4c7
Rentoll <kukue3636@gmail.com>
2019-06-01 08:10:14 +0300

    ES6

--- es6/core.js
+++ es6/core.js
@@ -3,32 +3,84 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(str) {
+    let arr = str.split(' ');
+    return `${arr[1]} ${arr[0]}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arr) {
+    let mySet = new Set(arr);
+    return Array.from(mySet);
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arr) {
+    if(arr.length === 0) {
+        return false;
+    }
+    let max = arr.reduce(function(max, cur) {
+        if (cur > max) return cur;
+        return max;
+    }, 0);
+    let min = arr.reduce(function(min, cur) {
+        if (cur < min) return cur;
+        return min;
+    }, max);
+    return max / min;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar() {
+    let arr = []
+    for (let i = 1; i < n; i++) {
+        if (i % 15 === 0) {
+          arr.push("FooBar");
+        } else if (i % 5 === 0) {
+          arr.push("Bar");
+        } else if (i % 3 === 0) {
+          arr.push("Foo");
+        } else {
+          arr.push(i);
+        }
+    }
+    return arr;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor() {
+        this.dict = new Map();
+    }
+    addWord(word, translate) {
+    if (typeof word != "string" || typeof translate != "string") {
+        console.log(`TypeError`);
+        return false;
+    }
+    if (this.dict.has(word) == true) {
+        console.log("Cлово уже присутствует в словаре!");
+        return false;
+    }
+    this.dict.set(word, translate);
+        return `Слово ${word} (${translate}) записано в словарь!`;
+    }
+    getWord(word) {
+        return this.dict.get(word);
+    }
+}
 
 module.exports = {
     fioToName,
