e9839aeb479dc16f3beb5783a4dd452ac3b21c28
Ilya Solovyov <ilyasolovyov52@yandex.ru>
2019-06-03 16:28:27 +0300

    I have done homework: closures and structures

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,32 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
 function sum(a) {
+  return function(b) {
+    return a + b;
+  }
+}
+
+//Пытался написать, но не получилсоь. Нашел вот это, оно работает, но я так до конца и непонял, как это работает, можешь объяснить???
+function sum(...parametrs) { 
+  let s = parametrs.reduce((a, b) => a + b); 
+  function innerSum(...innerParametrs) { 
+      return sum(...innerParametrs.concat(s)); 
+  }; 
+
+  innerSum.toString = innerSum.valueOf = function() { 
+      return s; 
+  } 
+  return innerSum;
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+  let sum=0;
+	let amount=0;
+  return function(next) {
+      sum += next;
+      amount++;
+      return sum / amount;
+  }
 }
 
 module.exports = {
--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,73 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  if (str.length>0){
+    str=str.substr(0,1).toUpperCase()+str.substr(1,str.length-1)
+  }
+  return str;
+}
+
+function kapitalizator(str) { 
+  newStr = ''; 
+  str = str.toLowerCase().split(' ',); //Разделяет строку на слова
+  for(var i=0; i < str.length; i++) { 
+      newStr += str[i].substring(0,1).toUpperCase() + str[i].substring(1,str[i].length) + ' '; //Капитализирует каждое слово
+  } 
+  return newStr; 
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+if (str.length>=n){
+  var resstr=str.substr(0,n);
+  return resstr+'...';
+}
+else
+{
+  return str;
+}
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for(key in obj) {
+    if(obj.hasOwnProperty(key))
+        return false;
+}
+return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for(key in obj) {
+    if(obj.hasOwnProperty(key)){
+	    if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+}
+return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr.reduce(reducer);
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+  var str2="";
+for (i=1;i<=str.length;i++){
+  str2+=str[str.length-i];
+}
+if (str2==str){
+  return 1;
+}
+else
+{
+  return 0;
+}
+}
 
 module.exports = {
   capitalize,
