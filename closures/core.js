7d3580895e83c0262eb6757598c784ea7f758088
geniusserg <50704370+geniusserg@users.noreply.github.com>
2019-06-06 23:16:45 +0300

    Danilov Sergey Closures

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,21 @@
-// Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
-function sum(a) {
+function sum(firstarg) {
+    var globalSum = firstarg;
+    function tempFunction(arg){
+        globalSum += arg;
+        return tempFunction;
+    }
+    tempFunction.toString = function(){ return globalSum }
+    return tempFunction;
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+    var globalResult = 0;
+    var globalLength = 0;
+    return function(nextvar){
+        globalResult = (globalResult*(globalLength++)+nextvar)/globalLength;
+        return globalResult;
+    }
 }
 
 module.exports = {
