f4a62edc2dde541b3713d1e18a98f4418ff42dac
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2019-06-08 17:43:16 +0300

    Update core.js

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,35 @@
+//Saratovcev Artem 18PI-1
+
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
-function sum(a) {
+function sum(a) 
+{
+  let currentSum = a; 
+
+  function fun(b) 
+  { 
+    currentSum += b; 
+    return fun; 
+  } 
+
+  fun.toString = function() 
+  { 
+    return currentSum; 
+  }; 
+
+  return fun; 
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
-function average() {
+function average() 
+{
+  let currentSum=0;
+  let n=0;
+  return function(a)
+  {
+    currentSum+=a;
+    n++;
+    return currentSum/n;
+  };
 }
 
 module.exports = {
