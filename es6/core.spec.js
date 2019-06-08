26125eb21874846c50d8ff9046abf5eff8306c13
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2019-06-08 18:56:41 +0300

    Update core.spec.js

--- es6/core.spec.js
+++ es6/core.spec.js
@@ -42,5 +42,38 @@ describe('es6', () => {
 
             assert.equal(!!dic, true);
         });
+
+        it("Don't set in case of repiated", () => {
+            const dic = new core.Dictionary({"дерево":"tree","дом":"house","Римма":"Rimma","дом":"house"});
+            assert.equal(dic.map.size,3);
+        });
+        it('Get word', () => {
+            const dic = new core.Dictionary({"дерево":"tree","дом":"house","Римма":"Rimma"});
+            assert.equal(dic.getTranslation("tree"),"дерево");
+        });
+        it('Set new word', () => {
+            const dic = new core.Dictionary({"дерево":"tree","дом":"house","Римма":"Rimma"});
+            dic.setWordAndTranslation({"прикол":"joke"});
+            assert.equal(dic.getTranslation("прикол"),"joke");
+        });
+        it("Don't set", () => {
+            const dic1 = new core.Dictionary({"дерево":"tree","дом":"house","Римма":"Rimma"});
+            const dic2 = new core.Dictionary({"дерево":"tree","дом":"house","Римма":"Rimma"});
+            dic2.setWordAndTranslation({"Римма":"Rimma"});
+            assert.equal(dic2.map.size==dic1.map.size,true);
+        });
     });
+
+    describe('#fooBar', () => {
+        it('if NaN return 0', () => {
+            assert.equal(core.fooBar('blablabla'),0);
+        });
+        it('if 0 return 0', () => {
+            assert.equal(core.fooBar(0),0);
+        });
+        it('Correct work with 15', () => {
+            assert.deepEqual(core.fooBar(15),[1,2,"Foo",4,"Bar","Foo",7,8,"Foo","Bar",11,"Foo",13,14,"FooBar"]);
+        });
+    });
+
 });
