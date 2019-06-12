7d5b49e3952925d5e9c343d4427ec3b8f9e58e9e
Olessia Martynyuk <45762301+aiiselo@users.noreply.github.com>
2019-06-12 22:50:07 +0300

    home task #5 without dictionary

--- es6/core.js
+++ es6/core.js
@@ -3,26 +3,65 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(FIO) {
+if (typeof FIO !== "string" ){
+        return false;
+    }
+    let [familia, imya] = FIO.split(" ");
+    return (imya + " " + familia);}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(array) {
+let set = new Set(array);
+    return [...set];}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(array) {
+    if(array.length > 0) {
+            let maxSalary = array.reduce(function(maxSalary, current) {
+                if (maxSalary > current) {
+                    return maxSalary;
+                }
+                else{
+                    return current;
+                }
+            }, 0);
+            let minSalary = array.reduce(function(minSalary, current) {
+                if (minSalary < current) {
+                    return minSalary;
+                }
+                return current;
+            }, maxSalary);
+            return maxSalary / minSalary;
+        }
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+    let myArray = [];
+    for (let i = 1; i < n+1; i++) {
+        if (i % 15 == 0) {
+            myArray.push("FooBar");
+        } else if (i % 5 == 0) {
+            myArray.push("Bar");
+        } else if (i % 3 == 0) {
+            myArray.push("Foo");
+        } else {
+            myArray.push(i);
+        }
+    }
+    return myArray;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
