acb5eb48ca57e1edfeac67db553781aa4192045a
Ilya Solovyov <ilyasolovyov52@yandex.ru>
2019-06-13 18:21:34 +0300

    I have done es6 homework

--- es6/core.js
+++ es6/core.js
@@ -38,17 +38,85 @@ function calculateSalaryDifference(arrayOfSalaries) {
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+  let array = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      array.push("FooBar");
+    } else if (i % 3 === 0) {
+      array.push("Foo");
+    } else if (i % 5 === 0) {
+      array.push("Bar");
+    } else {
+      array.push(i);
+    }
+  }
+  return array;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+
+class Dictionary {
+  constructor() {
+    this._dict = new Map();
+  }
+
+  add(key, value) {
+    if (typeof key === "string" && typeof value === "string") {
+      if (this._dict.has(key)) {
+        console.log(
+          `Ключ (${key}) уже добавлен в словарь. Значение (${this._dict.get(
+            key
+          )}) было заменено на (${value}).`
+        );
+      }
+      this._dict.set(key, value);
+    } else {
+      console.error(`Неверный формат ключа (${key}, ${value}).`);
+    }
+  }
+
+  get(key) {
+    if (typeof key !== "string") {
+      console.error(`Неверный формат ключа (${key}).`);
+    } else {
+      return this._dict.get(key);
+    }
+  }
+
+  delete(key) {
+    if (typeof key !== "string") {
+      console.error(`Неверный формат ключа (${key}).`);
+    } else {
+      if (this._dict.has(key)) {
+        this._dict.delete(key);
+      } else {
+        console.error(`В словаре нет элемента (${key}).`);
+      }
+    }
+  }
+  find(key) {
+    if (typeof key !== "string") {
+      console.error(`Неверный формат ключа (${key}).`);
+    } else {
+      return this._dict.has(key);
+    }
+  }
+  empty() {
+    return this._dict.size === 0;
+  }
+  size() {
+    return this._dict.size;
+  }
+}
 
 module.exports = {
   fioToName,
   filterUnique,
   Dictionary,
-  calculateSalaryDifference
+  calculateSalaryDifference,
+  fooBar
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -1,46 +1,107 @@
-const assert = require('assert');
-const core = require('./core');
+const assert = require("assert");
+const core = require("./core");
 
-describe('es6', () => {
-    describe('#fioToName', () => {
-        it('ФИО в Имя Фамилия корректно', () => {
-            assert.equal(core.fioToName('Иванов Иван Иванович'), 'Иван Иванов');
-        });
-
-        it('ФИ в Имя Фамилия', () => {
-            assert.equal(core.fioToName('Петров Петр'), 'Петр Петров');
-        });
+describe("es6", () => {
+  describe("#fioToName", () => {
+    it("ФИО в Имя Фамилия корректно", () => {
+      assert.equal(core.fioToName("Иванов Иван Иванович"), "Иван Иванов");
     });
 
-    describe('#filterUnique', () => {
-        it('массив с уникальными равен сам себе', () => {
-            assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
-        });
+    it("ФИ в Имя Фамилия", () => {
+      assert.equal(core.fioToName("Петров Петр"), "Петр Петров");
+    });
+  });
 
-        it('массив с неуникальными отфильтрован', () => {
-            assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
-        });
-
-        it('пустой массив', () => {
-            assert.deepEqual(core.filterUnique([]), []);
-        });
+  describe("#filterUnique", () => {
+    it("массив с уникальными равен сам себе", () => {
+      assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
     });
 
-    describe('#calculateSalaryDifference', () => {
-        it('считает разницу корректно', () => {
-            assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
-        });
-
-        it('на пустой массив возвращается falsy значение', () => {
-            assert.equal(!!core.calculateSalaryDifference([]), false);
-        });
+    it("массив с неуникальными отфильтрован", () => {
+      assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
     });
 
-    describe('#Dictionary', () => {
-        it('экземпляр класса создается', () => {
-            const dic = new core.Dictionary();
-
-            assert.equal(!!dic, true);
-        });
+    it("пустой массив", () => {
+      assert.deepEqual(core.filterUnique([]), []);
     });
+  });
+
+  describe("#calculateSalaryDifference", () => {
+    it("считает разницу корректно", () => {
+      assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
+    });
+
+    it("на пустой массив возвращается falsy значение", () => {
+      assert.equal(!!core.calculateSalaryDifference([]), false);
+    });
+  });
+
+  describe("#fooBar", () => {
+    it("корректно работает без чисел, кратных 3 и 5", () => {
+      assert.deepEqual(core.fooBar(2), [1, 2]);
+    });
+
+    it("корректно работает, если есть числа, кратные 3", () => {
+      assert.deepEqual(core.fooBar(3), [1, 2, "Foo"]);
+    });
+
+    it("корректно работает, если есть числа, кратные 5", () => {
+      assert.deepEqual(core.fooBar(5), [1, 2, "Foo", 4, "Bar"]);
+    });
+
+    it("корректно работает при любом наборе знаячений", () => {
+      assert.deepEqual(core.fooBar(16), [
+        1,
+        2,
+        "Foo",
+        4,
+        "Bar",
+        "Foo",
+        7,
+        8,
+        "Foo",
+        "Bar",
+        11,
+        "Foo",
+        13,
+        14,
+        "FooBar",
+        16
+      ]);
+    });
+  });
+
+  describe("#Dictionary", () => {
+    const translator = new core.Dictionary();
+
+    it("экземпляр класса создается", () => {
+      assert.equal(!!translator, true);
+    });
+
+    translator.add("Мама", "Mother");
+    translator.add("Папа", "Father");
+    translator.add("Бабушка", "Grandmother");
+
+    it("Добавление работает", () => {
+      assert.equal(translator.get("Мама"), "Mother");
+    });
+
+    it("Изменение работает", () => {
+      translator.add("Мама", "Mum");
+      assert.equal(translator.get("Мама"), "Mum");
+    });
+
+    it("Поиск ключа работает", () => {
+      assert.equal(translator.find("Мама"), true);
+    });
+
+    it("Удаление работает", () => {
+      translator.delete("Папа");
+      assert.equal(translator.get("Папа"), undefined);
+    });
+
+    it("Размер словаря считается правильно", () => {
+      assert.equal(translator.size(), 2);
+    });
+  });
 });
