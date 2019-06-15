eac71c6b2525bb4b11c9a7c3f8ab73187adc9993
BlackLunx <46784716+BlackLunx@users.noreply.github.com>
2019-06-15 16:49:06 +0300

    Update core.js

--- oop/core.js
+++ oop/core.js
@@ -1,13 +1,16 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
 class Point {
-  constructor(x, y) {
-    this.x = x === undefined ? 0 : x;
-    this.y = y === undefined ? 0 : y;
+  constructor(x = 0, y = 0) {
+    this._array = [];
+    this._array.push(x);
+    this._array.push(y);
+    this.x = x;
+    this.y = y;
   }
   distance() {
     return Math.sqrt(
-      this.reduce(function(squares, curSquare) {
+      this._array.reduce(function(squares, curSquare) {
         return squares + curSquare * curSquare;
       })
     );
@@ -16,28 +19,27 @@ class Point {
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
 class Point3D extends Point {
-  constructor(x, y, z) {
+  constructor(x = 0, y = 0, z = 0) {
     super(x, y);
-    this.z = z === undefined ? 0 : z;
+    this._array.push(z);
+    this.z = z;
   }
 }
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
 class Queue {
   constructor() {
-    this.queue = [];
+    this._queue = [];
   }
   push(newItem) {
-    queue.push(newItem);
+    this._queue.push(newItem);
+  }
+  isEmpty() {
+    return this._queue.length === 0;
   }
   pop() {
-    if (!queue.length) {
-      queue.shift();
-    }
-  }
-  front() {
-    if (!queue.length) {
-      return queue[0];
+    if (!this._queue.isEmpty()) {
+      return this._queue.shift();
     }
   }
 }
