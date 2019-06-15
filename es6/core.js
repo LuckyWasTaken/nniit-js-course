ad67c118cf2b23ee08e7b3a320c14a8ed038eb54
BlackLunx <46784716+BlackLunx@users.noreply.github.com>
2019-06-15 17:46:34 +0300

    задание es6

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,99 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToNam(fio) {
+  const [lastName, firstName] = string.split(" ");
+  return lastName + " " + firstName;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(array) {
+  const set = new Set(array);
+  return Array.from(set);
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(array) {
+  const [mini, maxi] = array.reduce(
+    function([mini, maxi], current) {
+      mini = Math.min(mini, current);
+      maxi = Math.max(maxi, current);
+      return [mini, maxi];
+    },
+    [array[0], array[0]]
+  );
+  return maxi / mini;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+  let array = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 == 0) {
+      array.push("FooBar");
+      continue;
+    }
+    if (i % 3 == 0) {
+      array.push("Foo");
+      continue;
+    }
+    if (i % 5 == 0) {
+      array.push("Bar");
+      continue;
+    }
+    array.push(i);
+  }
+  return array;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+  constructor() {
+    this._dictionary = new Map();
+  }
+  isString(str) {
+    if (typeof str != "string") {
+      console.log("Please, check the data entered");
+      return false;
+    }
+    return true;
+  }
+  add(word, interpretation) {
+    if (this.isString(word) && this.isString(interpretation)) {
+      this._dictionary.set(word, interpretation);
+    }
+  }
+  delete(word) {
+    if (this.isString(word) && this._dictionary.has(word)) {
+      this._dictionary.delete(word);
+      return;
+    }
+  }
+  get(word) {
+    if (this.isString(word) && this._dictionary.has(word)) {
+      return this._dictionary.get(word);
+    }
+    return "WARNING! There is no such word in the dictionary.";
+  }
+}
 
 module.exports = {
-    fioToName,
-    filterUnique,
-    Dictionary,
-    calculateSalaryDifference
+  fioToName,
+  filterUnique,
+  Dictionary,
+  calculateSalaryDifference,
+  fooBar
 };
