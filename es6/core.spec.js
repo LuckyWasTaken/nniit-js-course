73673f46676c2f37b413e41041ac7d4b06a2fbf6
BlackLunx <46784716+BlackLunx@users.noreply.github.com>
2019-06-15 17:46:11 +0300

    Обновил тесты для es6

--- es6/core.spec.js
+++ es6/core.spec.js
@@ -1,46 +1,81 @@
-const assert = require('assert');
-const core = require('./core');
+const assert = require("assert");
+const core = require("./core");
 
-describe('es6', () => {
-    describe('#fioToName', () => {
-        it('ФИО в Имя Фамилия корректно', () => {
-            assert.equal(core.fioToName('Иванов Иван Иванович'), 'Иван Иванов');
-        });
-
-        it('ФИ в Имя Фамилия', () => {
-            assert.equal(core.fioToName('Петров Петр'), 'Петр Петров');
-        });
+describe("es6", () => {
+  describe("#fioToName", () => {
+    it("ФИО в Имя Фамилия корректно", () => {
+      assert.equal(core.fioToName("Иванов Иван Иванович"), "Иван Иванов");
     });
 
-    describe('#filterUnique', () => {
-        it('массив с уникальными равен сам себе', () => {
-            assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
-        });
+    it("ФИ в Имя Фамилия", () => {
+      assert.equal(core.fioToName("Петров Петр"), "Петр Петров");
+    });
+  });
 
-        it('массив с неуникальными отфильтрован', () => {
-            assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
-        });
-
-        it('пустой массив', () => {
-            assert.deepEqual(core.filterUnique([]), []);
-        });
+  describe("#filterUnique", () => {
+    it("массив с уникальными равен сам себе", () => {
+      assert.deepEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
     });
 
-    describe('#calculateSalaryDifference', () => {
-        it('считает разницу корректно', () => {
-            assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
-        });
-
-        it('на пустой массив возвращается falsy значение', () => {
-            assert.equal(!!core.calculateSalaryDifference([]), false);
-        });
+    it("массив с неуникальными отфильтрован", () => {
+      assert.deepEqual(core.filterUnique([1, 1, 1, 1]), [1]);
     });
 
-    describe('#Dictionary', () => {
-        it('экземпляр класса создается', () => {
-            const dic = new core.Dictionary();
-
-            assert.equal(!!dic, true);
-        });
+    it("пустой массив", () => {
+      assert.deepEqual(core.filterUnique([]), []);
     });
+  });
+  describe("#fooBar", () => {
+    it("пустой массив", () => {
+      assert.deepEqual(core.fooBar(0), []);
+    });
+    it("при n равном 3", () => {
+      assert.deepEqual(core.fooBar(3), [1, 2, "Foo"]);
+    });
+    it("при n равном 5", () => {
+      assert.deepEqual(core.fooBar(5), [1, 2, "Foo", 4, "Bar"]);
+    });
+    it("при n равном 15", () => {
+      assert.deepEqual(core.fooBar(15), [
+        1,
+        2,
+        "Foo",
+        4,
+        "Bar",
+        "Foo",
+        7,
+        8,
+        "Foo",
+        "Bar",
+        11,
+        "Foo",
+        13,
+        14,
+        "FooBar"
+      ]);
+    });
+  });
+  describe("#calculateSalaryDifference", () => {
+    it("считает разницу корректно", () => {
+      assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
+    });
+
+    it("на пустой массив возвращается falsy значение", () => {
+      assert.equal(!!core.calculateSalaryDifference([]), false);
+    });
+  });
+
+  describe("#Dictionary", () => {
+    it("экземпляр класса создается", () => {
+      const dic = new core.Dictionary();
+
+      assert.equal(!!dic, true);
+    });
+    it("проверка на введение не string", () =>{
+        const dic = new core.Dictionary();
+        const dic2 = new core.Dictionary();
+        dic.add(1, 2);
+        assert.equal(dic, dic2);
+    });
+  });
 });
