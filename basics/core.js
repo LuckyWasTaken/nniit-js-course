dca19d547e9e0dbaa6f5662520d516c2db0979af
Roman Ershov <>
2019-06-16 22:16:08 +0300

    Ершов Роман 18ПИ-2 (basics, structures, closures)

--- basics/core.js
+++ basics/core.js
@@ -1,31 +1,78 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+	return ((n | 0) === n);
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+	let arr = [], index = 0;
+	for (num = 2; num <= 20; num++) {
+		if (!(num % 2)){
+			arr[index] = num;
+			index++;
+		}
+	}
+	return arr;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+	let sum = 0;
+	for (i = 1; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+	if (n > 1) {
+		return recSumTo (n-1) + n;
+	}
+	return 1;
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+	let product = 1;
+	if (n >= 0) {
+		for (i = 1; i <= n; i++) {
+			product *= i;
+		}
+		return product;
+	}
+	return 0;
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+	if (n < 1) {
+		return false;
+	}
+	let binary = 1;
+	while (binary < n) {
+		binary *= 2;
+		if (binary > n) {
+			return false;
+		}
+	}
+	return true;
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+	if (n < 3) {
+		return 1;
+	}
+	let fiboPrev = 1;
+	let fiboCurrent = 1;
+	for (i = 3; i <= n; i++) {
+		fiboCurrent += fiboPrev;
+		fiboPrev = fiboCurrent - fiboPrev;
+	}
+	return fiboCurrent;
 }
 
 module.exports = {
-    isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
+	isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
 }
--- structures/core.js
+++ structures/core.js
@@ -1,26 +1,58 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+	let newStr = "";
+	for (let i = 0; i < str.length; i++){
+		if (i === 0 || str[i - 1] === ' '){
+			newStr += str[i].toUpperCase();
+		}
+		else{
+			newStr += str[i];
+		}
+	}
+	return newStr;
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+	if (str.length <= n) {
+		return str;
+	}
+	return str.substr (0, n) + "...";
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+	for (let key in obj){
+		return false;
+	}
+	return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+	for (let key in obj) {
+		if (isFinite(obj[key])) {
+		 	obj[key] *= 2;
+		}
+	}
+	return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+	return arr.reduce(function (sum, element) {return sum + element});
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+	return (str === str.split("").reverse().join(""));
+}
 
 module.exports = {
-  capitalize,
-  truncate,
-  isEmpty,
-  multiply,
-  sumArr,
-  isPali
+	capitalize,
+	truncate,
+	isEmpty,
+	multiply,
+	sumArr,
+	isPali
 };
