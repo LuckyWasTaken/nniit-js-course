2b84d6b416d21789f2a8667c127853582eb0b37e
rainbowsock <50048459+rainbowsock@users.noreply.github.com>
2019-06-18 19:40:30 +0300

    Update core.spec.js

--- es6/core.spec.js
+++ es6/core.spec.js
@@ -42,5 +42,37 @@ describe('es6', () => {
 
             assert.equal(!!dic, true);
         });
+        it("Слова не устанавливаются повторно", () => {
+            const dic = new core.Dictionary({"сердце":"heart","исключение":"exclusion","Иисус":"Jesus","сердце":"heart"});
+            assert.equal(dic.map.size,3);
+        });
+        it('Вывод перевода', () => {
+            const dic = new core.Dictionary({"сердце":"heart","исключение":"exclusion","Иисус":"Jesus"});
+            assert.equal(dic.getTranslation("heart"),"сердце");
+        });
+        it('Добавляет новое слово', () => {
+            const dic = new core.Dictionary({"сердце":"heart","исключение":"exclusion","Иисус":"Jesus"});
+            dic.setWordAndTranslation({"работа":"job"});
+            assert.equal(dic.getTranslation("работа"),"job");
+        });
+        it("Не добавляет слова второй раз", () => {
+            const dic1 = new core.Dictionary({"сердце":"heart","исключение":"exclusion","Иисус":"Jesus"});
+            const dic2 = new core.Dictionary({"сердце":"heart","исключение":"exclusion","Иисус":"Jesus"});
+            dic2.setWordAndTranslation({"Иисус":"Jesus"});
+            assert.equal(dic2.map.size==dic1.map.size,true);
+        });
     });
+        
+    describe('#fooBar', () => {
+        it('Если не число - возвращет ноль', () => {
+            assert.equal(core.fooBar('гыг'),0);
+        });
+        it('Если ноль -  возвращает ноль', () => {
+            assert.equal(core.fooBar(0),0);
+        });
+        it('Корректно работает с 15', () => {
+            assert.deepEqual(core.fooBar(15),[1,2,"Foo",4,"Bar","Foo",7,8,"Foo","Bar",11,"Foo",13,14,"FooBar"]);
+        });
+    });
+    
 });
