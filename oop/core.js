e9afdb0867d325b72a1c235cf8d774745ee57d55
Roman Ershov <>
2019-06-18 16:43:06 +0300

    Homework (OOP)

--- oop/core.js
+++ oop/core.js
@@ -1,13 +1,55 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
+class Point {
+    constructor(x = 0, y = 0){
+        if(isFinite(x)){
+            this.x = x;
+        }
+        if(isFinite(y)){
+            this.y = y;
+        }
+    }
+    get distanceFromCenter() {
+        let sumOfSquares = 0;
+        for (let key in this){
+            sumOfSquares += Math.pow(this[key], 2);
+        }
+        return Math.sqrt(sumOfSquares);
+    }
+}
 
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
+class Point3D extends Point {
+    constructor(x, y, z = 0){
+        super(x, y);
+        if(isFinite(z)){
+            this.z = z;
+        }
+    }
+}
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
+class Queue {
+    constructor(){
+        this._array = [];
+        this._size = 0;
+    }
+    push(elem){
+        this._array.push(elem);
+        this._size++;
+    }
+    get pop(){
+        if(this._size){
+            this._size--;
+            return this._array.shift();
+        }
+        return undefined;
+    }
+    get top(){
+        return test._array[0];
+    }
+}
 
 module.exports = {
     Point,
