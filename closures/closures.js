d396828fb2cd11ee6b26aee5049dd34788da0512
maxalex5000 <51835040+maxalex5000@users.noreply.github.com>
2019-06-24 19:27:19 +0300

    closures

---
+++ closures/closures.js
@@ -0,0 +1,29 @@
+// Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
+function sum(a) {
+function sum(a) 
+{
+  return function(b)
+  {
+  return a+b;
+  }
+}
+
+// Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
+function average() {
+function average() 
+{
+  let count=0;
+  let sum=0;
+
+  return function(number)
+  {
+    sum += number;
+    count++;
+    return sum/count;
+  }
+}
+
+module.exports = {
+  sum,
+  average
+};
