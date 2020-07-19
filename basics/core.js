56d8f1c7d41628dd4f6049cb309f93bbaf271ec8
Marina Postnikova <nerra@yandex.ru>
2020-07-19 18:24:18 +0300

    basics is ready

--- basics/core.js
+++ basics/core.js
@@ -1,29 +1,78 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    let number = (~~n);
+    if (number == n) {
+       return true;
+    } else {
+        return false;
+    }
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let array = new Array();
+    for (let i = 2; i<21; i++) {
+        if (i%2 === 0) {
+            array.push(i);
+        } 
+    }
+    return array;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    let sum = 0; 
+    for (let i = 0; i<=n; i++) {
+        sum+=i;
+    }
+    return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if (n === 1) {
+        return n;
+    } 
+    else {
+       return recSumTo(n-1) + n;  
+    }
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    if (n === 0 || n === 1)  {
+        return 1;
+    }
+    else { 
+        return (factorial(n-1) * n);
+    }
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    if (n <=0 ) {
+        return false;
+    }
+    while (n >2 ) {
+      if ( (n%2) == 0) {
+        n = n/2;
+        } 
+        else {
+        return false;
+        }
+    }  
+    return true; 
 }
+console.log(isBinary(50));
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 module.exports = {
