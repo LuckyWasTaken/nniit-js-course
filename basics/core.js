72f6c33dcb46ce91740b10acdc2151766fead6dc
Marina Postnikova <nerra@yandex.ru>
2020-07-21 22:18:00 +0300

    Add prettier and format code

--- basics/core.js
+++ basics/core.js
@@ -1,66 +1,69 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
-    const number = (~~n);
-    return (number === n)
+  const number = ~~n;
+  return number === n;
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
-    const array = [];
-    for (let i = 2; i<21; i++) {
-        if (i%2 === 0) {
-            array.push(i);
-        } 
+  const array = [];
+  for (let i = 2; i < 21; i++) {
+    if (i % 2 === 0) {
+      array.push(i);
     }
-    return array;
+  }
+  return array;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
-    let sum = 0; 
-    for (let i = 0; i<=n; i++) {
-        sum+=i;
-    }
-    return sum;
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
-    return (n === 1 ? n : recSumTo(n-1) + n);   
+  return n === 1 ? n : recSumTo(n - 1) + n;
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
-    return (n === 0 || n === 1 ? n : factorial(n-1) * n);
+  return n === 0 || n === 1 ? n : factorial(n - 1) * n;
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
-    if (n <=0 ) {
-        return false;
+  if (n <= 0) {
+    return false;
+  }
+  while (n > 2) {
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else {
+      return false;
     }
-    while (n >2 ) {
-      if ( (n%2) == 0) {
-        n = n/2;
-        } 
-        else {
-        return false;
-        }
-    }  
-    return true; 
+  }
+  return true;
 }
-
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
-    if (n === 1 || n === 2) {
-        return 1;
-    }
-    else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+  if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 }
 
 module.exports = {
-    isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
-}
+  isInteger,
+  even,
+  sumTo,
+  recSumTo,
+  factorial,
+  isBinary,
+  fibonacci,
+};
