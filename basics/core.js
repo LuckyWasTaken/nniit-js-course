046c535282357dd8ac12585588a6220b875b9a4e
SemKilyaev <darkvanted@gmail.com>
2020-07-22 16:07:33 +0300

    Basics is done P.s. Interesed in Frontend

--- basics/core.js
+++ basics/core.js
@@ -1,29 +1,72 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    /*if(parseInt(n)==parseFloat(n)) {
+        return true;
+    } else {
+        return false;
+    }*/
+    return (n ^ 0) === n;
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let list=[];
+    let finishNumber=20;
+    for(let startNumber=2; startNumber<=finishNumber; startNumber++){
+        if(startNumber%2==0){
+            list.push(startNumber);
+        } else {continue}
+        if(startNumber==finishNumber){
+            return list;
+        }
+    }
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    let result=0;
+    for(let count=0; count<=n; count++){
+        result=result+count;
+        if(count==n){
+            return result;
+        }
+    }
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if(n == 1){
+        return 1;
+    }
+    return n + recSumTo(n - 1);
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    let result=1;
+    if(n==0){
+        return 1 //Факториал 0=1
+    }
+    for(let startNumber=1; startNumber<=n; startNumber++){
+        result=result*startNumber;
+        if(startNumber==n){
+            return result;
+        }
+    }
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    return (n <=0) ? 0 : (n & (n - 1)) == 0;
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    if(n == 1 || n == 2){
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 module.exports = {
