822be3aa9010022ffae6e8a3b2d2d66a4c71d25a
Marina Postnikova <nerra@yandex.ru>
2020-07-25 17:44:59 +0300

    The task 2 closures is ready

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,40 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
 function sum(a) {
+  return function summarize(b) {
+    return a+b;
+  };
+}
+
+//Task* first option
+function sumAnyQuantityOfNumbers1(a) {
+  let result = a;
+  return function summarize(b) {
+    result += b;
+    console.log(result);
+    return summarize;
+  };
+}
+
+//Task* second option
+function sumQuantityOfNumbers2(a) {
+  let result = a;
+  return function summarize(b) {
+    result += b;
+    return (result);
+  };
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+  const array = [];
+  return function calcucateAvarage(number) {
+    array.push(number);
+    let result = array.reduce(function(sum, current) {
+      return sum + current;
+
+    }, 0);
+    return (result / (array.length));
+  };
 }
 
 module.exports = {
