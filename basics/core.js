b95bb1c696f04e1c6d6a0aa8c36b1289da002bd1
tatyanashashalina <shash.tan@mail.ru>
2020-07-27 21:37:50 +0300

    First task Shashalina

--- basics/core.js
+++ basics/core.js
@@ -1,30 +1,83 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    let a = n & n;
+
+    if (a != n)
+        return false;
+
+    else
+        return true;
+
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let arr = new Array();
+
+    let i = 2;
+    while (i != 22) {
+        arr.push(i);
+        i += 2;
+    }
+    // console.log(arr);
+
+    return arr;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    let i = 0;
+    let sum = 0
+    while (i != n + 1) {
+        sum += i;
+        i++;
+    }
+    return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if (n == 0) {
+        return n;
+    }
+    else
+        return n + recSumTo(n - 1);
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    let i = 1;
+    let mul = 1
+    while (i != n + 1) {
+        mul *= i;
+        i++;
+    }
+    return mul;
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    let num = Math.log2(n);
+    //console.log(num);
+    let check = num & num;
+
+    if (check != num)
+        return false;
+
+    else
+        return true;
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    if (n == 2 || n == 1) {
+        return 1;
+    }
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+
 
 module.exports = {
     isInteger, even, sumTo, recSumTo, factorial, isBinary, fibonacci
