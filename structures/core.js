aea65adc9fe75efe0842396a4f81a78d4d678ab2
Koeco97 <komissarovaolesya@yandex.ru>
2020-07-28 20:55:06 +0300

    structures are done

--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,57 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  if(str.length===0)
+    return "";
+  let separator = " ";
+  let arrayOfStrings = str.split(separator);
+  let firstLetter="";
+  let partOfWord="";
+  let newStr="";
+  for (let i = 0; i<arrayOfStrings.length; i++){
+     firstLetter = arrayOfStrings[i][0];
+     partOfWord = arrayOfStrings[i];
+     newStr+=firstLetter.toUpperCase()+partOfWord.substr(1);
+  }
+  return newStr;
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+  if(n > str.length)
+    return str;
+  return str.substr(0,n)+"..."
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for(key in obj)
+    return false;
+  return true;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for(key in obj)
+    if(typeof obj[key] == "number")
+        obj[key]*=2;
+  return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr.reduce(reducer);
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+  arr = str.split("");
+  reverseArr = str.split("").reverse();
+  for(let i = 0; i< str.length; i++)
+    if(arr[i]!=reverseArr[i])
+      return false;
+  return true;
+}
 
 module.exports = {
   capitalize,
