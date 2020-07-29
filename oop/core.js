2c09192397331e3b9b6f05a43e83d0715aada30f
Andrey Gubin <agubin@pop-os.localdomain>
2020-07-29 23:04:26 +0300

    A.Gubin 'oop' tasks

--- oop/core.js
+++ oop/core.js
@@ -1,13 +1,41 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
+class Point {
+    constructor(x, y) {
+        this.x = x ? x : 0
+        this.y = y ? y : 0
+    }
+    
+    toCenter = function() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2)
+    }
+}
 
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
+class Point3D extends Point {
+    constructor(x, y, z) {
+        super(x, y)
+        this.z = z ? z : 0
+    }
+
+    toCenter() {
+        return Math.sqrt(super.toCenter() ** 2 + this.z ** 2)
+    }
+}
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
+class Queue {
+    constructor() {
+        this.queue = []
+    }
+    push(a) {
+        this.queue.push(a)
+    }
+    pull() {
+        return this.queue.shift()
+    }
+}
 
 module.exports = {
     Point,
