627bdf34ecb4ae5f22f9f71bafd6906eb8340bc7
Bear346 <57682132+Bear346@users.noreply.github.com>
2020-07-30 14:35:08 +0300

    Update core.js

--- closures/core.js
+++ closures/core.js
@@ -1,15 +1,1 @@
-function sum(a) {
-	return function(b){return a+b;}
-}
 
-// Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
-function average() {
-	let count = 0;
-	let avg = 0;
-	return function(b){
-		avg+=b;
-		count++;
-		avg/=count;
-		return avg;
-	}
-}
