505a3e697c1030da6b0e9715a9dd9c7d288cdf60
Alexander Karpov <cool_07@mail.ru>
2020-08-03 12:15:24 +0300

    Basics, closures, oop, structures

--- basics/core.js
+++ basics/core.js
@@ -1,29 +1,62 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    return(n ^ 0) === n;
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let arr = []
+    for (let i=1; i<=20;i++){
+        if (i%2 == 0)
+            arr.push(i);
+    }
+    return arr;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    let sum = 0;
+    for (let i=1; i<=n;i++){
+        sum = sum+i;}
+        return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if (n === 1){
+        return 1;
+    }
+    return recSumTo(n - 1) + n;
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    if (n == 0) {
+    return 1;}
+    return factorial(n - 1) * n;
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    if (n == 0){
+        return false;
+    }
+    if (n == 1){
+        return true;
+    }
+    let num = n;
+    while (num / 2 > 1){
+        if (num % 2 != 0){
+            return false;
+        }
+        num /= 2;
+    }
+    return true;
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = {
