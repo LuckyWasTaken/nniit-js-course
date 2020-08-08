f570e3c0760197dcebc01fb6e9e84f54f4ede30a
Koeco97 <komissarovaolesya@yandex.ru>
2020-08-08 18:23:10 +0300

    es6 is done

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,94 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(fio) {
+    let fullFio = String(fio).split(" ");
+    let surname = fullFio[0];
+    let name = fullFio[1];
+    return `${name} ${surname}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(numArr) {
+    numSet = new Set(numArr);
+    return Array.from(numSet);
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(salary) {
+    if(salary.length===0){
+        console.log('no salary entered')
+        return false;
+    }
+    salary.sort((a, b) => a - b);
+    return salary[salary.length-1]/salary[0];
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+    let sequence = function* (n) {
+        for (let i = 1; i <= n; i++) {
+            if(i % 15 === 0){
+                yield "FooBar";
+            }
+            else if (i % 5 === 0){
+                yield "Bar";
+            }
+            else if (i % 3 === 0){
+                yield "Foo"
+            } 
+            else yield i;
+        }
+    }
+    let numbers = [ ...sequence(n)]
+    return numbers;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor(){
+        this.dictionary = new Map;
+    }
+    add(word, translation){
+        if(typeof(word)!="string" || typeof(translation)!="string"){
+            console.log('incorrect input')
+            return false
+        }
+        this.dictionary.set(word, translation);
+    }
+    getTranslation(word){
+        if(!this.dictionary.has(word)){
+            console.log('this word is not found')
+            return false;
+        }
+        return this.dictionary.get(word);
+    }
+    delete(word){
+        if(!this.dictionary.has(word)){
+            console.log('this word is not found')
+            return false;
+        }
+        return this.dictionary.delete(word);
+    }
+}
 
 module.exports = {
     fioToName,
     filterUnique,
     Dictionary,
-    calculateSalaryDifference
+    calculateSalaryDifference,
+    fooBar
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -31,16 +31,62 @@ describe('es6', () => {
             assert.equal(core.calculateSalaryDifference([1, 2, 3]), 3);
         });
 
-        it('на пустой массив возвращается falsy значение', () => {
-            assert.equal(!!core.calculateSalaryDifference([]), false);
+        it('на пустой массив возвращается false значение', () => {
+            assert.equal(core.calculateSalaryDifference([]), false);
+        });
+    });
+
+    describe('#fooBar', () => {
+        it('при делении на 3 возвращается Foo', () => {
+            assert.deepEqual(core.fooBar(4),[1,2,"Foo",4]);
+        });
+
+        it('при делении на 5 возвращается Foo', () => {
+            assert.deepEqual(core.fooBar(5),[1,2,"Foo",4, "Bar"]);
+        });
+
+        it('при делении на 15 возвращается FooBar', () => {
+            assert.deepEqual(core.fooBar(15),[1, 2, "Foo", 4, "Bar", "Foo", 7, 8, "Foo", "Bar", 11, "Foo", 13, 14, "FooBar"]);
         });
     });
 
     describe('#Dictionary', () => {
         it('экземпляр класса создается', () => {
             const dic = new core.Dictionary();
-
             assert.equal(!!dic, true);
+        });
+        it('при добавлении не строки в качестве key возвращается false', () => {
+            const dic = new core.Dictionary();
+            assert.equal(dic.add(1, 'one'), false);
+        });
+        it('при добавлении не строки в качестве value возвращается false', () => {
+            const dic = new core.Dictionary();
+            assert.equal(dic.add('one', 1), false);
+        });
+        it('При добавлении строки в качестве key и value не возвращается false', () => {
+            const dic = new core.Dictionary();
+            assert.notEqual(dic.add('one', 'один'), false);
+        });
+        it('Возвращается корректное значение по ключу', () => {
+            const dic = new core.Dictionary();
+            dic.add('one', 'один');
+            assert.equal(dic.getTranslation('one'), 'один');
+        });
+        it('Если перевод не найден, возвращается false', () => {
+            const dic = new core.Dictionary();
+            dic.add('one', 'один');
+            assert.equal(dic.getTranslation('two'), false);
+        });
+        it('слово корректно удаляется', () => {
+            const dic = new core.Dictionary();
+            dic.add('one', 'один');
+            dic.delete('one');
+            assert.equal(dic.getTranslation('one'), false);
+        });
+        it('При удалении несуществующего слова возвращается false', () => {
+            const dic = new core.Dictionary();
+            dic.add('one', 'один');
+            assert.equal(dic.delete('two'), false);
         });
     });
 });
