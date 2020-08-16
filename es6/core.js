3f7b1c4cd717d71f3037d499c14d0fec90cefc5e
asincov <sashasintsov@mail.ru>
2020-08-16 17:05:13 +0300

    a.sintsov es6

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,148 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(strFio) {
+    const arrFio = strFio.split(" ");
+    const [surname,name] = arrFio; 
+    return `${name} ${surname}`;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arr) {
+    const set = new Set(arr);
+    return [...set]
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arr) {
+    if (arr.length == 0){
+        return false;
+    }
+    const min = arr.reduce((minTemp, currentValue) => currentValue < minTemp ? currentValue : minTemp);
+    const max = arr.reduce((maxTemp, currentValue) => currentValue > maxTemp ? currentValue : maxTemp);
+    return max / min;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+    const fun = () => Array.from({length: n}, (v, k) => {
+        return (++k % 3 == 0) ? "foo" : ((k % 5) == 0 ? "bar" : k);
+    });
+    return fun();
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+
+    _wrong_symbols = `${"1234567890!@#$%^&*()_+-=\|/~,.?;:'"}${'"`'}${" "}`;
+    _wrong_symbol_alert = "Alert: wrong symbol in the word";
+    _word_is_not_found_alert = "Alert: word is not found";
+    _word_added_alert = "Word added successfully";
+    _word_deleted_alert = "Word deleted successfully";
+
+    constructor(){
+        this._words = new Map();
+        this._message = "";
+    }
+
+    _getMessage(){
+        return this._message;
+    }
+
+    _setMessage(strMessage){
+        this._message = this._message.replace(this._message, strMessage);
+    }
+
+    _checkWord(strWord){
+        for (let symb of this._wrong_symbols){
+            if (strWord.includes(symb)){
+                throw new Error("Wrong symbol in word");
+            }
+        }
+    }
+
+    _checkIncluded(strWord){
+        if (!(this._words.has(strWord))){
+            throw new Error("Word is not found");
+        }
+    }
+
+    addWord(strWord, strDescription){
+        try{
+            this._checkWord(strWord);
+            this._words.set(strWord, strDescription);
+            this._setMessage(this._word_added_alert);
+        }
+        catch(e){
+            if(e.message == "Wrong symbol in word"){
+                this._setMessage(this._wrong_symbol_alert);
+            }
+            else {
+                throw e;
+            }
+        }
+        console.log(this._getMessage());
+    }
+
+    deleteWord(strWord){
+        try{
+            this._checkIncluded(strWord);
+            this._words.delete(strWord);
+            this._setMessage(this._word_deleted_alert);
+        }
+        catch(e){
+            if (e.message == "Word is not found"){
+                this._setMessage(this._word_is_not_found_alert);
+            }
+            else {
+                throw e;
+            }
+        }
+        console.log(this._getMessage());
+    }
+
+    printDescription(strWord){
+        try{
+            this._checkIncluded(strWord);
+            this._setMessage(`${strWord} - ${this._words.get(strWord)}`);
+        }
+        catch(e){
+            if (e.message == "Word is not found"){
+                this._setMessage(this._word_is_not_found_alert);
+            }
+            else {
+                throw e;
+            }
+        }
+        console.log(this._getMessage());
+    }
+
+    printAll(){
+        for (let word of this._words.keys()){
+            this.printDescription(word);
+        }
+    }
+}
 
 module.exports = {
     fioToName,
     filterUnique,
     Dictionary,
-    calculateSalaryDifference
+    calculateSalaryDifference,
+    fooBar
 };
+
+console.log(calculateSalaryDifference([2,3,4,5]));
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const core = require('./core');
+const { fooBar } = require('./core');
 
 describe('es6', () => {
     describe('#fioToName', () => {
@@ -39,8 +40,69 @@ describe('es6', () => {
     describe('#Dictionary', () => {
         it('экземпляр класса создается', () => {
             const dic = new core.Dictionary();
-
             assert.equal(!!dic, true);
+        });
+
+        it('добавляется описание разрешенного слова', () => {
+            const dic = new core.Dictionary();
+            dic.addWord("London", "The capital of Greate Britain");
+            assert.equal(dic._message, "Word added successfully");
+            assert.equal(dic._words.size, 1);
+            dic.addWord("Moscow", "The capital of Russian Federation");
+            assert.equal(dic._message, "Word added successfully");
+            assert.equal(dic._words.size, 2);
+        });
+
+        it('попытка добавить слово с запрещенными символами', () => {
+            const dic = new core.Dictionary();
+            dic.addWord("Lo`ndon", "The capital of Greate Britain");
+            assert.equal(dic._words.size, 0);
+            assert.equal(dic._message, "Alert: wrong symbol in the word");
+        });
+
+
+        it('поиск и печатать описания входящего в словарь слова', () => {
+            const dic = new core.Dictionary();
+            dic.addWord("London", "The capital of Greate Britain");
+            dic.addWord("Moscow", "The capital of Russian Federation");
+            dic.printDescription("London");
+            assert.equal(dic._message, "London - The capital of Greate Britain");
+        });
+
+        it('попытка напечатать описание не входящего в словарь слова', () => {
+            const dic = new core.Dictionary();
+            dic.addWord("London", "The capital of Greate Britain");
+            dic.printDescription("LondonQWE");
+            assert.equal(dic._message, "Alert: word is not found");
+        });
+
+        it('удаление описания входящего в словарь слова', () => {
+            const dic = new core.Dictionary();
+            dic.addWord("London", "The capital of Greate Britain");
+            assert.equal(dic._message, "Word added successfully");
+            assert.equal(dic._words.size, 1);
+            dic.deleteWord("London");
+            assert.equal(dic._message, "Word deleted successfully");
+            assert.equal(dic._words.size, 0);
+        });
+
+    });
+
+    describe('#fooBar', () => {
+        it('устанавливаем тип объекта - массив', () => {
+            assert.equal(toString.call(fooBar(10)), "[object Array]");
+        });
+
+        it('сравниваем длину массива', () => {
+            assert.equal(fooBar(10).length, 10);
+        });
+
+        it('сравниваем элементы массива', () => {
+            const arr = [1,2,"foo",4,"bar","foo",7,8,"foo","bar"];
+            let i = 0;
+            for (let n of fooBar(10)){
+                assert.equal(n, arr[i++]);
+            }
         });
     });
 });
