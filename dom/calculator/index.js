f8b59d24fc10d6be22fba0081e087721804bd1b3
Alexander Karpov <cool_07@mail.ru>
2020-08-28 21:06:19 +0300

    Karpov_Dom

---
+++ dom/calculator/index.js
@@ -0,0 +1,24 @@
+function calculate()
+{
+    let left = document.getElementById("left");
+    let right = document.getElementById("right")
+    let result = document.getElementById("result")
+
+    switch (document.getElementById("action").value) 
+    {
+        case '+':
+            result.innerHTML = (Number(left.value) + Number(right.value));
+            break;
+            case '-':
+            result.innerHTML = (left.value - right.value);
+            break;
+        case '*':
+            result.innerHTML = (left.value * right.value);
+            break;
+        case '/':
+            result.innerHTML = (left.value / right.value);
+            break;
+        default:
+            break;
+    }
+}
---
+++ dom/changecolor/index.js
@@ -0,0 +1,9 @@
+const headertext = document.getElementById("text")
+function swapToGreen()
+{
+   headertext.style.color = "#059b00";
+}
+function swapToRed()
+{
+   headertext.style.color = "#b90303";
+} 
