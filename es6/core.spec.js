ba2f8112186ed32087b88fe4c2c108226962af2c
TwinSss <chulan4ikmoi91@yandex.ru>
2020-09-01 21:12:35 +0300

    es6 is done

--- es6/core.spec.js
+++ es6/core.spec.js
@@ -39,8 +39,42 @@ describe('es6', () => {
     describe('#Dictionary', () => {
         it('экземпляр класса создается', () => {
             const dic = new core.Dictionary();
+            dic.addWord("one", "1");
+            dic.addWord(1, "one");
+            dic.getValue("one");
+            dic.getValue("two");
+            dic.deleteWord("one");
+            dic.getValue("two");
 
             assert.equal(!!dic, true);
         });
+        it('добавляем  ключ : значение', () => {
+            const dic = new core.Dictionary();
+            assert.equal(dic.addWord("one", "1"), true);
+            assert.equal(dic.addWord(1, "one"), false);
+        });
+        it('поиск по ключу', () => {
+            const dic = new core.Dictionary();
+            dic.addWord("one", "1");
+
+            assert.equal(dic.getValue("one"), "1");
+            assert.equal(dic.getValue("two"), undefined);
+        });
+        it('удаление ключ : значение', () => {
+            const dic = new core.Dictionary();
+            dic.addWord("one", "1");
+
+            assert.equal(dic.deleteWord("one"), true);
+            assert.equal(dic.deleteWord("two"), false);
+        });
     });
+
+    describe('#fooBar', () => {
+        it('возвращение корректного массива', () => {
+           assert.deepEqual(core.fooBar(16), [1, 2, "Foo", 4, "Bar", "Foo", 7, 8, "Foo", "Bar", 11, "Foo", 13, 14, "FooBar", 16]);
+        });
+        it('на 0 значение возвращается пустой массив', () => {
+            assert.deepEqual(core.fooBar(0), []);
+        });
+    });    
 });
