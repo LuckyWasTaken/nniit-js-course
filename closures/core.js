64a2b1c548cb9987cd95f681144a9212fee20a4e
SemKilyaev <darkvanted@gmail.com>
2020-09-07 17:15:41 +0300

    Closures is done

--- closures/core.js
+++ closures/core.js
@@ -1,9 +1,17 @@
 // Напишите функцию, суммирующую два числа, так, что sum(a)(b) == a + b (задание с очень жирной звездочкой на поразбираться - суммируйте любое количество чисел)
 function sum(a) {
+  return (b) => a + b;
 }
 
 // Напишите замыкание, которое будет принимать число и считать среднее арифметическое от всех переданных ранее чисел (если задание не до конца понятно - подглядите тесты :) )
 function average() {
+  let count = 0;
+  let average_number = 0;
+  return function(number){
+    count++;
+    average_number+=number;
+    return average_number/count;
+  }
 }
 
 module.exports = {
