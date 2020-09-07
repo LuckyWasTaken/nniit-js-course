eda9288d7551001cad023fd18953d9db0f891f8f
asincov <sashasintsov@mail.ru>
2020-09-07 23:09:37 +0300

    mixed edited after comments

--- mixed/index.js
+++ mixed/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("form");
-    const input = document.getElementById("inputedUserId");
-    const tableData = document.getElementById("tableData");
-    const flushButton = document.getElementById("flushButton");
+    const input = document.getElementById("form-inputed-user-id");
+    const tableData = document.getElementById("table-data");
+    const flushButton = document.getElementById("form-button-flush");
     let tablerows = [];
 
     form.addEventListener("submit", e => {
@@ -14,11 +14,16 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((data) => {
             flushTablerows(tablerows);
             let foundData = [];
-            if (inputedUserId.value ==""){
+            if (input.value ==""){
                 foundData = [...data];
             }
             else {
-                foundData = data.filter(function(datum){return datum[`userId`].toString() == input.value});
+                foundData = data.filter(function(datum){return datum[`userId`].toString() == input.value})
+                // for (datum of data){
+                //     if (datum[`userId`].toString() == input.value){
+                //         foundData.push(datum);
+                //     }
+                //}
             }
             createTableData(foundData, tableData, tablerows);
         });
@@ -64,6 +69,5 @@ const flushTablerows = (tablerows) => {
 }
 
 const flushInput = (input) => {
-    console.log("fluush")  
     input.value = "";
-} 
+}
