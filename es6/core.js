772705556266ca0a9297c9cb8f53d198d0efabfe
SemKilyaev <darkvanted@gmail.com>
2020-09-09 14:45:30 +0300

    es6 is done

--- es6/core.js
+++ es6/core.js
@@ -3,36 +3,90 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName() {}
+function fioToName(str) {
+    let spl = str.split(" ");
+    let result = spl[1]+" "+spl[0];
+    return result;
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique() {}
+function filterUnique(arr) {
+    return [...new Set(arr)]
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 // присмотритесь к методу .reduce
-function calculateSalaryDifference() {}
+function calculateSalaryDifference(arr) {
+    let max = Math.max.apply(null, arr);
+    let min = Math.min.apply(null, arr);
+    return max / min;
+}
 
 // Задачка с собеседований fooBar
 // Напишите функцию, которая принимает n
 // возвращает массив чисел от 1 до n, где вместо чисел, которые делятся на 3 — "Foo",
 // чисел, которые делятся на 5 — "Bar", а на 15 — "FooBar"
 // * покройте тестами
-function fooBar() {}
+function fooBar(n) {
+    let result = [];
+    for(let i=1; i<n; i++){
+        if(i%15===0){
+            result.push('Foobar');
+        } else if(i%5===0){
+            result.push('Bar');
+        } else if(i%3===0){
+            result.push('Foo')
+        } else{
+            result.push(i)
+        }
+    }
+    return result;
+}
 
 // Реализуйте класс "словарь слов"
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor(){
+        this.my_map = new Map();
+    }
+    add_key_and_value(key, value){
+        this.key=key;
+        this.value=value;
+        if(typeof this.key === 'string' && typeof this.value === 'string'){
+            return this.my_map.set(this.key, this.value)
+        }
+    }
+    get_size(){
+        return this.my_map.size;
+    }
+    clear_Dict(){
+        return this.my_map.clear();
+    }
+    has_key(key){
+        this.key = key;
+        return this.my_map.has(this.key);
+    }
+    delete_value(key){
+        this.key = key;
+        return this.my_map.delete(this.key);
+    }
+    get_value(key){
+        this.key = key;
+        return this.my_map.get(this.key);
+    }
+}
 
 module.exports = {
     fioToName,
     filterUnique,
     Dictionary,
-    calculateSalaryDifference
+    calculateSalaryDifference,
+    fooBar
 };
--- es6/core.spec.js
+++ es6/core.spec.js
@@ -36,11 +36,57 @@ describe('es6', () => {
         });
     });
 
+    describe('#fooBar', () => {
+        it('Выдает верный массив', () => {
+            assert.deepEqual(core.fooBar(16), [1, 2, "Foo", 4, "Bar", "Foo", 7, 8, "Foo", "Bar", 11, "Foo", 13, 14, "Foobar"]);
+        });
+    });
+
     describe('#Dictionary', () => {
         it('экземпляр класса создается', () => {
             const dic = new core.Dictionary();
 
             assert.equal(!!dic, true);
+        });
+        it('узнать размер словаря', () => {
+            const dic = new core.Dictionary();
+            dic.add_key_and_value('123', '345');
+            assert.equal(dic.get_size(), 1);
+        });
+        it('удаление значения по ключу', () => {
+            const dic = new core.Dictionary();
+            dic.add_key_and_value('123', '345');
+            dic.add_key_and_value('12', 'hello');
+            assert.equal(dic.delete_value('12'), true);
+        });
+        it('проверка по ключу', () => {
+            const dic = new core.Dictionary();
+            dic.add_key_and_value('123', '345');
+            dic.add_key_and_value('12', 'hello');
+            assert.equal(dic.has_key('12'), true);
+        });
+        it('получение значения', () => {
+            const dic = new core.Dictionary();
+            dic.add_key_and_value('123', '345');
+            dic.add_key_and_value('12', 'hello');
+            assert.equal(dic.get_value('12'), 'hello');
+        });
+        it('очистка словаря', () => {
+            const dic = new core.Dictionary();
+            dic.add_key_and_value('123', '345');
+            dic.add_key_and_value('12', 'hello');
+            dic.clear_Dict()
+            assert.equal(dic.get_size(), 0);
+        });
+        it('добавление элементов', () => {
+            const dic = new core.Dictionary();
+            dic.add_key_and_value('123', '345');
+            dic.add_key_and_value('12', 'hello');
+            assert.equal(dic.get_size(), 2);
+            assert.equal(dic.has_key('123'), true);
+            assert.equal(dic.has_key('12'), true);
+            assert.equal(dic.get_value('123'), '345');
+            assert.equal(dic.get_value('12'), 'hello');
         });
     });
 });
