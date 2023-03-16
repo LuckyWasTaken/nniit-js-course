1e00e92966fd3d7dbbf2a09b9cedb7acfd916853
Egor <edbaturin@edu.hse.ru>
2023-03-16 22:15:29 +0300

    first js homework

--- basics/core.js
+++ basics/core.js
@@ -1,29 +1,56 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    return (n | 0) === n;
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let result = [];
+    for (let i = 2; i <= 20; i+= 2) {
+        result.push(i);
+    }
+    return result;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    result = 0;
+    for (let i = 1; i <= n; ++i) {
+        result += i;
+    }
+    return result;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if (n === 1){
+        return 1;
+    }
+    return n + recSumTo(n - 1);
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    result = 1;
+    for (let i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    return n && (n & (n - 1)) === 0;
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    } 
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 module.exports = {
