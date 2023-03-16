ec1b7f447a16238665f58fd51444a7344e5405b8
Den-Shab <79015516+Den-Shab@users.noreply.github.com>
2023-03-16 14:52:22 +0300

    Update core.js

--- basics/core.js
+++ basics/core.js
@@ -5,7 +5,7 @@ function isInteger(n) {
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
-    let arr = []
+    let arr = [];
     for (let i = 2; i <= 20; i += 2) {
         arr.push(i);
     }
@@ -18,45 +18,45 @@ function sumTo(n) {
     for(let i = 1; i <= n; i++){
         sum += i;
     }
-    return sum;
+    return sum
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
     if (n == 0){
-        return 0;
+        return 0
     }
-    return (n + recSumTo(n - 1));
+    return (n + recSumTo(n - 1))
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
     if (n == 1){
-        return 1;
+        return 1
     }
-    return factorial(n - 1) * n;
+    return factorial(n - 1) * n
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
     if (n == 0){
-        return false;
+        return false
     }
-    return ((n & (n - 1)) == 0);
+    return ((n & (n - 1)) == 0)
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
     if (n == 0){
-        return 0;
+        return 0
     }
     if (n == 1){ 
-        return 1;
+        return 1
     }
     if(n == 2){ 
-        return 1;
+        return 1
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 module.exports = {
