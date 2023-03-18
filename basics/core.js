45e152189007d40560be8fa7dcbcfc09fc52e004
mingboyev.kh <mingboyev.kh@gmail.com>
2023-03-18 19:51:39 +0300

    code refactor

--- basics/core.js
+++ basics/core.js
@@ -1,44 +1,43 @@
-// Функция, которая проверяет, является ли переданное значение целым числом
+//Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
-    return Number.isInteger(n);
-  }
+  return Number.isInteger(n);
+}
   
-  // Функция, которая возвращает массив четных чисел от 2 до 20 включительно
+//Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
-    return Array.from({length: 10}, (_, i) => (i + 1) * 2);
+  return Array.from({length: 10}, (_, i) => (i + 1) * 2);
 }
   
-  // Функция, которая считает сумму чисел до заданного, используя цикл
+//Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function sumTo(n) {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-      sum += i;
-    }
-    return sum;
-}
-  
-  // Функция, которая считает сумму чисел до заданного, используя рекурсию
-function recSumTo(n) {
-    return n === 1 ? 1 : n + recSumTo(n - 1);
-}
-  
-  // Функция, которая считает факториал заданного числа
-  
-function factorial(n) {
-    return n === 0 ? 1 : n * factorial(n - 1);
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
   }
-  
-  // Функция, которая определяет, является ли число двойкой, возведенной в степень
-function isBinary(n) {
-    return Number.isInteger(Math.log2(n));
+  return sum;
 }
   
-  // Функция, которая находит N-е число Фибоначчи
+//Напишите функцию, считающую сумму чисел до заданного используя рекурсию
+function recSumTo(n) {
+  return n === 1 ? 1 : n + recSumTo(n - 1);
+}
+  
+//Напишите функцию, считающую факториал заданного числа
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+  
+//Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
+function isBinary(n) {
+  return Number.isInteger(Math.log2(n));
+}
+  
+//Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
-    if (n <= 1) {
+  if (n <= 1) {
       return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = {
