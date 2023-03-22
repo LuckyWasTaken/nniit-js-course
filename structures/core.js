e6570e885ce41e99232d19ccaf7b90019d539d0b
Den-Shab <79015516+Den-Shab@users.noreply.github.com>
2023-03-22 15:11:09 +0300

    Update core.js

--- structures/core.js
+++ structures/core.js
@@ -1,11 +1,33 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  
+  return str.split('').map(Mapper).join('')
+
+  function Mapper(cur, index, string) {
+
+    if (index == 0 || string[index - 1] == " ") {
+        return cur.toUpperCase()
+    } else {
+        return cur
+    }
+  }
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) { 
+  if (n <= str.length) {
+    return str.slice(0, n) + "..."
+  }
+  return str
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for (let item in obj) {
+      return false
+    }
+    return true
+}
 
 //Напишите функцию, умножающую численные свойства на 2
 function multiply(obj) {}
