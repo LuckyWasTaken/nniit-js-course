2704beba0a947180f8f9797a99721f3050bbe45d
golub.m.e <golub.m.e@effective-group.ru>
2023-03-24 20:15:13 +0300

    js h/w №2-3 + убрала лишние else из h/w №1

--- oop/core.js
+++ oop/core.js
@@ -1,16 +1,45 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
+class Point {
+  constructor(x = 0, y = 0) {
+    this.x = x
+    this.y = y
+  }
+  distanceToСenter() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2)
+  }
+}
 
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
+class Point3D extends Point {
+  constructor(x, y, z = 0) {
+    super(x, y)
+    this.z = z
+  }
+}
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
+class Queue {
+  constructor() {
+    this.items = []
+  }
+  enqueue(item) {
+    this.items.push(item)
+  }
+  dequeue() {
+    if (this.isEmpty()) {
+      return null
+    }
+    return this.items.shift()
+  }
+  isEmpty() {
+    return this.items.length === 0
+  }
+}
 
 module.exports = {
-    Point,
-    Point3D,
-    Queue,
-};
+  Point,
+  Point3D,
+  Queue,
+}
--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,56 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  if (!str) {
+    return str
+  }
+  let strWords = str.split(' ')
+  for (let i = 0; i < strWords.length; i++) {
+    strWords[i] = strWords[i][0].toUpperCase() + strWords[i].substr(1)
+  }
+  return strWords.join(' ')
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+  if (str.length > n) {
+    return str.slice(0, n) + '...'
+  }
+  return str
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      return false
+    }
+  }
+  return true
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key) && typeof obj[key] === 'number') {
+      obj[key] *= 2
+    }
+  }
+  return obj
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  return arr.reduce(function (total, current) {
+    return total + current
+  }, 0)
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+  str = str.replace(/\s/g, '').toLowerCase()
+
+  return str === str.split('').reverse().join('')
+}
 
 module.exports = {
   capitalize,
@@ -22,5 +58,5 @@ module.exports = {
   isEmpty,
   multiply,
   sumArr,
-  isPali
-};
+  isPali,
+}
