9b10d998f9badecc5f4a5486d95cb533525ff04e
Lul1ch <84387626+Lul1ch@users.noreply.github.com>
2023-03-24 22:40:05 +0300

    HW2 Eregin20Pi2

--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,52 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+	const words = str.split(' ');
+	const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+	return capitalizedWords.join(' ');
+	
+	return capitalizedWords;
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+	if (str.length > n) {
+		str = str.slice(0, n) + "...";
+	}
+	return str;
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+	let isEmpty = JSON.stringify(obj) === '{}'
+	return isEmpty;
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+	for (const [key, value] of Object.entries(obj)) {
+        if (typeof(value) === 'number') {
+            obj[key] *= 2
+        }
+    }
+	return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+	let res = arr.reduce((sum, current) => sum + current, 0);
+	return res;
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+	str = str.toLowerCase()
+	for (var i = 0; i < str.length/2; i++) {
+		if (str[i] != str[str.length - i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
 
 module.exports = {
   capitalize,
