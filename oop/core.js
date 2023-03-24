734c937f5d639a7f44a90ede930e8e4ca832760e
unknown <Masloboynikov.Igor@yandex.ru>
2023-03-25 02:19:25 +0300

    HomeWork Done 25.03.2023

--- oop/core.js
+++ oop/core.js
@@ -1,16 +1,52 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
-
-// Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
-// который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
-
-// Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
-
-module.exports = {
+class Point {
+    constructor(x = 0, y = 0) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    distanceBetweenCenter() {
+      return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+  }
+  
+  // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
+  // который будет наследоваться от точки в двумерном пространстве
+  class Point3D extends Point {
+    constructor(x, y, z = 0) {
+      super(x, y);
+      this.z = z;
+    }
+  
+    distanceBetweenCenter() {
+      return Math.sqrt(
+        Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2)
+      );
+    }
+  }
+  
+  // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
+  class Queue {
+    constructor() {
+      this.elements = {};
+      this.head = 0;
+      this.tail = 0;
+    }
+    enqueue(element) {
+      this.elements[this.tail] = element;
+      this.tail++;
+    }
+    dequeue() {
+      const item = this.elements[this.head];
+      delete this.elements[this.head];
+      this.head++;
+      return item;
+    }
+  }
+  
+  module.exports = {
     Point,
     Point3D,
     Queue,
-};
+  };
--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,57 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function capitalize_sentence(sentence) {
+  list_items = [];
+  for (const item of sentence.split(" ")) {
+    list_items.push(capitalize(item));
+  }
+  return list_items.join(" ");
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+  var str_slice = str.slice(0, n);
+  if (n > str.length) {
+    return str_slice;
+  } else {
+    return str_slice + "...";
+  }
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for (const [key, value] of Object.entries(obj)) {
+    if (typeof value == "number") {
+      obj[key] *= 2;
+    }
+  }
+  return obj;
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  return arr.reduce(function (a, b) {
+    return a + b;
+  });
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+  str = str.toLowerCase();
+  var reverse_str = str.split("").reverse().join("");
+  if (str == reverse_str) {
+    return true;
+  } else return false;
+}
+
 
 module.exports = {
   capitalize,
