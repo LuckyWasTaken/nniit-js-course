2c091676c7d2e0e3d628030deb2cf0da0838e99d
vsmorkovkin <thegramanhuth@mail.ru>
2023-03-25 00:08:54 +0300

    HM4

--- structures/core.js
+++ structures/core.js
@@ -1,20 +1,44 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+  return str && str[0].toUpperCase() + str.slice(1)
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+  return (n <= str.length) ? str.substr(0, n) + "..." : str
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0 && obj.constructor === Object
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+  for (let key in obj) {
+    if (obj[key].constructor === Number) {
+      obj[key] *= 2
+    }
+  }
+  return obj
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+  return arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  )
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+  for (let i = 0; i < Math.floor(str.length / 2); ++i) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false
+    }
+  }
+  return true
+}
 
 module.exports = {
   capitalize,
