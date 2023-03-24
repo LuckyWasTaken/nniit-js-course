0f573e74b05d515abb4d89c1c7f76cf4e08b5a17
lpmaltina <lyudmilamaltina@gmail.com>
2023-03-24 14:37:45 +0300

    Update Queue class
    1) Make properties 'data', 'front' and 'back' private.
    
    2) Add isEmpty(), size(), getFrontElement(), getBackElement() methods.
    
    3) If the queue is empty, methods pop(), getFrontElement(), getBackElement() return null.

--- oop/core.js
+++ oop/core.js
@@ -26,24 +26,60 @@ class Point3D extends Point {
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
 class Queue {
+    #data;
+    #front;
+    #back;
     
     constructor() {
-        this.data = [];
-        this.front = 0;
-        this.back = 0;
+        this.#data = [];
+        this.#front = 0;
+        this.#back = 0;
+    }
+    
+    // Если очередь пустая, возвращаем true, иначе - false
+    isEmpty(){
+        return this.#back === this.#front;
+    }
+    
+    // Возврашаем количество элементов в очереди
+    size(){
+        return this.#back - this.#front;
     }
     
     // Добавляем элемент в конец очереди
     enqueue(el){
-        this.data.push(el);
-        this.back += 1;
+        this.#data.push(el);
+        this.#back += 1;
     }
     
-    // Удаляем элемент из начала очереди и возвращаем его
+    // Если очередь непустая,
+    // удаляем элемент из её начала и возвращаем этот элемент.
+    // Иначе возвращаем null
     pop(){
-        const el = this.data[this.front];
-        this.front += 1;
-        return el;
+        if (!this.isEmpty()){
+            const el = this.#data[this.#front];
+            this.#front += 1;
+            return el;
+        }
+        return null;
+    }
+    
+    // Если очередь непустая, возвращаем её начальный элемент.
+    // Иначе возвращаем null
+    getFrontElement(){
+        if (!this.isEmpty()){
+            return this.#data[this.#front];
+        }
+        return null;
+    }
+    
+    // Если очередь непустая, возвращаем её последний элемент.
+    // Иначе возвращаем null
+    getBackElement(){
+        if (!this.isEmpty()){
+            return this.#data[this.#back - 1];
+        }
+        return null;
     }
 }
 
