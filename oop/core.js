e01f14eed7751c0680c1f081e4e82d6d0842eb15
Nikita Sigal <nikitasigal@icloud.com>
2023-03-26 00:51:39 +0300

    Homework 2 – Structures, closures and OOP

--- oop/core.js
+++ oop/core.js
@@ -1,13 +1,45 @@
 // Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
 // Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
-class Point {}
+class Point {
+    constructor(x, y) {
+        this.x = x || 0
+        this.y = y || 0
+    }
+
+    distance() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2)
+    }
+}
 
 // Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
 // который будет наследоваться от точки в двумерном пространстве
-class Point3D extends Point {}
+class Point3D extends Point {
+    constructor(x, y, z) {
+        super(x, y);
+        this.z = z || 0
+    }
+}
 
 // Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала
-class Queue {}
+class Queue {
+    constructor() {
+        this.arr = []
+    }
+
+    push(x) {
+        this.arr.push(x)
+    }
+
+    pop() {
+        if (this.arr.length > 0) {
+            let res = this.arr[0]
+            this.arr.shift()
+            return res
+        } else {
+            return 0
+        }
+    }
+}
 
 module.exports = {
     Point,
--- structures/core.js
+++ structures/core.js
@@ -1,26 +1,57 @@
 //Напишите функцию, которая делает первый символ строки заглавным (задание со звездочкой - капитализируйте каждое слово)
-function capitalize(str) {}
+function capitalize(str) {
+    return str.split(' ').map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+}
 
 //Напишите функцию, которая вернет строку, усеченную до n символов и добавляет в конец многоточие (если n > длина строки - ничего делать не надо)
-function truncate(str, n) {}
+function truncate(str, n) {
+    if (n > str.length) {
+        return str
+    } else {
+        return str.slice(0, n) + '...'
+    }
+}
 
 //Определите, пуст ли объект
-function isEmpty(obj) {}
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0
+}
 
 //Напишите функцию, умножающую численные свойства на 2
-function multiply(obj) {}
+function multiply(obj) {
+    Object.keys(obj).map((key) => {
+        if (typeof obj[key] === 'number') {
+            obj[key] *= 2
+        }
+    })
+    return obj
+}
 
 //Напишите функцию, считающую сумму всех элементов массива (желательно использовать reduce)
-function sumArr(arr) {}
+function sumArr(arr) {
+    return arr.reduce((acc, cur) => {
+        return acc + cur
+    }, 0)
+}
 
 //Напишите функцию, определяющую, является ли данное слово палиндромом
-function isPali(str) {}
+function isPali(str) {
+    let ans = true
+    for (let i = 0; i <= str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            ans = false
+        }
+    }
+    return ans
+}
 
 module.exports = {
-  capitalize,
-  truncate,
-  isEmpty,
-  multiply,
-  sumArr,
-  isPali
+    capitalize,
+    truncate,
+    isEmpty,
+    multiply,
+    sumArr,
+    isPali
 };
