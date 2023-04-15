2a2f6302a52a9bdbf388e5243926510be22e41e8
lpmaltina <lyudmilamaltina@gmail.com>
2023-04-15 22:07:50 +0300

    Task 1: refactor the code and prettify the result

--- dom/task1_colors/index.js
+++ dom/task1_colors/index.js
@@ -1,10 +1,12 @@
 function setColor(){
-    var newColor = map.get(this.id);
+    var newColor = colors.get(this.id);
     document.getElementById('text').style.color = newColor;
 }
 
-var map = new Map();
-map.set('redButton', '#ff0000');
-map.set('blueButton', '#0000ff');
-document.getElementById('redButton').onclick = setColor;
-document.getElementById('blueButton').onclick = setColor;
+var colors = new Map();
+colors.set('redButton', '#ff0000');
+colors.set('blueButton', '#0000ff');
+
+for (const buttonName of colors.keys()) {
+    document.getElementById(buttonName).onclick = setColor;
+}
