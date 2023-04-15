18f487a23997c15430f157b8355a90d49ac4e712
lpmaltina <lyudmilamaltina@gmail.com>
2023-04-15 22:25:27 +0300

    Do task 2 (calculator)

---
+++ dom/task2_calculator/index.js
@@ -0,0 +1,30 @@
+document.getElementById('resultButton').onclick = function(){
+    var number1 = parseFloat(document.getElementById('number1').value);
+    var number2 = parseFloat(document.getElementById('number2').value);
+    var el = document.getElementById("operator");
+    var operator = el.options[el.selectedIndex].value;
+    var result;
+    
+    if (Number.isNaN(number1) || Number.isNaN(number2)){
+        result = "Incorrect input!";
+    }
+    else if (operator === '+'){
+        result = number1 + number2;
+    }
+    else if (operator === '-'){
+        result = number1 - number2;
+    }
+    else if (operator === '*'){
+        result = number1 * number2;
+    }
+    else if (operator === '/'){
+        if (number2 === 0){
+           result = "Cannot divide by zero!";
+        }
+        else {
+            result = number1 / number2;
+        }
+    }
+    
+    document.getElementById("result").innerHTML = result;
+}
