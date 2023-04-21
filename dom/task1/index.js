6bcf7e868b575abf209bde00e65347f9ec3419eb
gsgol <77744037+gsgol@users.noreply.github.com>
2023-04-21 13:59:00 +0300

    Update index.js

--- dom/task1/index.js
+++ dom/task1/index.js
@@ -1,4 +1,4 @@
 function changeColor(color) {
-  document.getElementById('text1').style.color = color
-  document.getElementById('text2').style.color = color
+  document.getElementById('text1').style.color = color;
+  document.getElementById('text2').style.color = color;
 }
