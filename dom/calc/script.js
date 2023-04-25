4e06ea2839b21952987ce63e554ac029e32be385
SolidSolar <46050106+SolidSolar@users.noreply.github.com>
2023-04-25 20:10:25 +0300

    feat: added tasks dom and calc

---
+++ dom/calc/script.js
@@ -0,0 +1,48 @@
+﻿let x = document.getElementById("x");
+let y = document.getElementById("y");
+
+let result = document.getElementById("result");
+let dart_monkey = document.getElementById("dart_monkey");
+let operation = document.getElementById("operation");
+
+function getResult() {
+    switch (operation.value) {
+        case "+":
+            return Number(x.value) + Number(y.value);
+            break;
+        case "-":
+            return Number(x.value) - Number(y.value);
+            break;
+        case "*":
+            return Number(x.value) * Number(y.value);
+            break;
+        case "\\":
+            return Number(x.value) / Number(y.value);
+        case "^":
+            return Number(x.value) ** Number(y.value);
+            break;
+    }
+}
+
+function update() {
+    result.textContent = getResult();
+}
+
+operation.addEventListener("change", update);
+x.addEventListener("input", update);
+y.addEventListener("input", update);
+
+dart_monkey.textContent =
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢀⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣰⣿⣿⣿⣿⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⡿⠿⢿⣿⣿⣶⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⢼⡟⠉⣻⣿⣿⡏⠰⣷⠀⢹⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⢻⣷⡀⠙⣻⣿⣿⣄⣠⣴⡿⠋⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⠀⣭⣉⣛⣻⣿⣿⣿⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⢠⠞⢡⣽⣿⣿⠿⢻⣿⣿⣿⣏⣿⣿⣿⣧⣤⣤⣤⣄⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠘⣴⡨⠛⠋⠁⠀⣼⣿⣿⣿⡟⣿⣿⣿⣿⣯⢈⣿⣿⠂⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠘⠃⠀⠀⠀⢀⣤⣿⣷⡜⣿⣧⡉⠉⠙⠋⠁⠈⠉⠁⠀⠀⠀⠀⠀\n" +
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⢠⣾⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
+    "\n обезьянка считает, что \n";
