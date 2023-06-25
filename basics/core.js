580beb0ebc1856bbace7833bc8d745b68f08c302
Алексей Шишкин <86236873+Shish-ok@users.noreply.github.com>
2023-06-25 11:55:10 +0300

    lab1.js

--- basics/core.js
+++ basics/core.js
@@ -1,29 +1,57 @@
 //Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
 function isInteger(n) {
+    return (n | 0) === n;
 }
 
 //Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
 function even() {
+    let a = [];
+    for (let i = 2; i <= 20; i += 2) {
+        a.push(i);
+    }
+    return a
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя цикл
 function sumTo(n) {
+    let sum = 0;
+    
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 //Напишите функцию, считающую сумму чисел до заданного используя рекурсию
 function recSumTo(n) {
+    if (n === 1) {
+        return 1
+    }
+    return (n + recSumTo(n - 1))
 }
 
 //Напишите функцию, считающую факториал заданного числа
 function factorial(n) {
+    if (n === 1) {
+        return 1
+    }
+    return n * factorial(n - 1)
 }
 
 //Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
 function isBinary(n) {
+    if (n === 0) {
+        return false
+    }
+    return (n & (n - 1)) === 0
 }
 
 //Напишите функцию, которая находит N-е число Фибоначчи
 function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = {
